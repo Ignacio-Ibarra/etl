f83cbacd64a1e6d599a2ed8716624e89e5524470
jgjuara <jg.juara@gmail.com>
2024-01-05 13:34:31 -0300

    reordeno donde va el script de crear_directorios

--- scripts/crear_directorios.R
+++ scripts/crear_directorios.R
@@ -5,7 +5,9 @@ library(googledrive)
 library(purrr)
 
 # definir mail del usuario gmail con el que leer el drive
-drive_auth(email = "")
+# 
+
+googledrive::drive_auth(email = Sys.getenv("USER_GMAIL"))
 
 df <- drive_ls(path = as_id("https://drive.google.com/drive/folders/16Out5kOds2kfsbudRvSoHGHsDfCml1p0"))
 
@@ -13,14 +15,18 @@ id_subtopicos <- df[df$name == "SUBTOPICOS",]$id
 
 subtopicos <- drive_ls(id_subtopicos)
 
-map(subtopicos$name, \(x) {
+walk(subtopicos$name, \(x) {
   if (!dir.exists(glue::glue("scripts/{x}"))) {
     
     dir.create(glue::glue("scripts/{x}"))
   }
 })
 
-map(subtopicos$name, \(x) {
+if (!dir.exists("data")) {dir.create("data")}
+
+walk(subtopicos$name, \(x) {
+  
+  
   if (!dir.exists(glue::glue("data/{x}"))) {
     
     dir.create(glue::glue("data/{x}"))
