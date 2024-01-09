997a1c36cf337782d2ad0f98574850dfd6d48d75
jgjuara <jg.juara@gmail.com>
2024-01-09 11:09:50 -0300

    seteo el auth de drive en el script main

--- scripts/main.R
+++ scripts/main.R
@@ -1,3 +1,5 @@
+googledrive::drive_auth(email = Sys.getenv("USER_GMAIL"))
+
 subtopicos <- sort(list.dirs("scripts/subtopicos/", full.names = F)[list.dirs("scripts/subtopicos/", full.names = F) != ""])
 
 subtopico <- subtopicos[subtopicos == "ACECON"]
