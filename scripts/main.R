4b5ece9c875d4785fff75a49af218308efaa24a6
jgjuara <jg.juara@gmail.com>
2024-01-09 12:33:33 -0300

    agrego lectura del diccionario consolidado_fundar_paises_agregaciones2

--- scripts/main.R
+++ scripts/main.R
@@ -4,6 +4,9 @@ source("scripts/aux_functions.R")
 # reutuliza la auth drive cacheada para el mail guardado como var de environ de R
 googledrive::drive_auth(email = Sys.getenv("USER_GMAIL"))
 
+# levanta diccionario de codigos iso y nombres de pais
+iso_countrycodes <- read.csv("https://docs.google.com/spreadsheets/d/1kK1Yu6gz5kEWe_i0vamiGttkXUH5H90e/export?format=csv&id=1kK1Yu6gz5kEWe_i0vamiGttkXUH5H90e&gid=808722347")
+iso_countrycodes <- iso_countrycodes %>% select(iso3, pais = iso3_desc_fundar)
 # levanta lista de subtopicos de la propia estructura de directorios
 subtopicos <- sort(list.dirs("scripts/subtopicos/", full.names = F)[list.dirs("scripts/subtopicos/", full.names = F) != ""])
 
