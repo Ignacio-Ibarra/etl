443ceb89b3ba460fce0424d55201635059860653
jgjuara <jg.juara@gmail.com>
2024-01-22 11:49:01 -0300

    agrego insumos de acecon parte 1

--- scripts/insumos.R
+++ scripts/insumos.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 
 
-# cuentas nacionales fundacion norte y sur 
+# cuentas nacionales fundacion norte y sur   -----------
 # ngdp_r = PIB moneda nacional constante 2004 (esta en miles)
 # ngdprpc = PIB per capita moneda nacional constante 2004
 
@@ -10,7 +10,7 @@ download.file("https://docs.google.com/spreadsheets/d/e/2PACX-1vTAGGfIqDw18YDI5z
               destfile = glue::glue("data/_INSUMOS/raw/cuentas-nacionales-fund-norte-y-sur.xlsx"))
 
 
-# IMF outlook database 
+# IMF outlook database   ----------- 
 # descargo la base entera por mayor facilidad de referencia
 # unidades 
 # "NGDP_R" (pib) esta en miles de millones de moneda nacional constantes (1e9)
@@ -22,7 +22,7 @@ download.file(url = "https://www.imf.org/-/media/Files/Publications/WEO/WEO-Data
 
 
 
-# Maddison database
+# Maddison database  ----------- 
 # GDP pc	Real GDP per capita in 2011$
 # Population	Population, mid-year (thousands)
 
@@ -32,3 +32,18 @@ download.file(url = "https://www.rug.nl/ggdc/historicaldevelopment/maddison/data
 
 # api undp human development report office -----------
 # https://api.hdrdata.org/swagger/index.html
+
+# oferta y demanda global trimestral INDEC cuentas nacionales  -----------
+oyd_cn_indec_url <- "https://www.indec.gob.ar/ftp/cuadros/economia/sh_oferta_demanda_12_23.xls"  
+
+download.file(url = oyd_cn_indec_url, 
+              mode = "wb", # archivos tipo xlsx requieren escritura tipo binaria
+              destfile = glue::glue("data/_INSUMOS/raw/sh_oferta_demanda_12_23.xls"))
+
+# proyeccion nacional poblacion indec   -----------
+pob_indec_url <- "https://www.indec.gob.ar/ftp/cuadros/poblacion/c1_proyecciones_nac_2010_2040.xls"
+
+
+download.file(url = pob_indec_url, 
+              mode = "wb", # archivos tipo xlsx requieren escritura tipo binaria
+              destfile = glue::glue("data/_INSUMOS/raw/c1_proyecciones_nac_2010_2040.xls"))
