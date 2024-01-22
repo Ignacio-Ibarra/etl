9499c56287a5cb3ea7c9d2aed295ad08c1ddf271
jgjuara <jg.juara@gmail.com>
2024-01-22 11:48:31 -0300

    setting de gdrive pasa a main flow

--- scripts/crear_directorios.R
+++ scripts/crear_directorios.R
@@ -4,16 +4,6 @@
 library(googledrive)
 library(purrr)
 
-# definir mail del usuario gmail con el que leer el drive
-# 
-
-googledrive::drive_auth(email = Sys.getenv("USER_GMAIL"))
-
-df <- drive_ls(path = as_id("https://drive.google.com/drive/folders/16Out5kOds2kfsbudRvSoHGHsDfCml1p0"))
-
-id_subtopicos <- df[df$name == "SUBTOPICOS",]$id
-
-subtopicos <- drive_ls(id_subtopicos)
 
 walk(subtopicos$name, \(x) {
   if (!dir.exists(glue::glue("scripts/subtopicos/{x}"))) {
