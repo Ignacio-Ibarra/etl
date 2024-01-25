32eea55b98171ed2aca5cb298e9a91bed8bfffcd
jgjuara <jg.juara@gmail.com>
2024-01-25 12:21:06 -0300

    ajusto llamada de funciones auxilares para incluir la incializacion de subtopico entre esas funciones

--- scripts/main.R
+++ scripts/main.R
@@ -1,9 +1,16 @@
 library(tidyverse)
 library(googledrive)
-
 # reutuliza la auth drive cacheada para el mail guardado como var de environ de R
 googledrive::drive_auth(email = Sys.getenv("USER_GMAIL"))
 
+
+source("scripts/aux_functions.R")
+
+
+
+# asignar var subtopico con el nombre del subtopico a trabajar
 subtopico <- "ACECON"
 
-source("scripts/subtopico_init.R")
+# asignar a outputs
+outputs <- subtopico_init(subtopico_nombre = subtopico, entrega = "primera_entrega")
+
