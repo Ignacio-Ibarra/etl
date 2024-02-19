1282b1e5e26c83b531ba9d9af3147c3a304f6d5b
jgjuara <jg.juara@gmail.com>
2024-02-19 12:41:20 -0300

    tomo como var global el id de drive

--- scripts/main.R
+++ scripts/main.R
@@ -1,7 +1,11 @@
 library(tidyverse)
 library(googledrive)
+
 # reutuliza la auth drive cacheada para el mail guardado como var de environ de R
 googledrive::drive_auth(email = Sys.getenv("USER_GMAIL"))
+
+# id de la carpeta que contiene Argendata en el drive
+argendata_drive <- Sys.getenv("ARGENDATA_DRIVE")
 
 
 source("scripts/aux_functions.R")
