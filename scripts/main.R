18f52c8846f5d3631ee362f7b784e39dc4b3946e
jgjuara <jg.juara@gmail.com>
2024-03-08 15:57:19 -0300

    reordena main y scripts de creacion de directorios y metadata pasa a scripts/utils

--- scripts/main.R
+++ scripts/main.R
@@ -1,19 +1,17 @@
 library(tidyverse)
 library(googledrive)
 library(googlesheets4)
+library(argendataR)
 
 # reutuliza la auth drive cacheada para el mail guardado como var de environ de R
 googledrive::drive_auth(email = Sys.getenv("USER_GMAIL"))
 googlesheets4::gs4_auth(email = Sys.getenv("USER_GMAIL"))
 
 
-walk(.x = list.files("scripts/funciones/", full.names = T), source)
-
-# fuentes <- read_csv("data/_FUENTES/fuentes_lista.csv")
-# 
-# # asignar var subtopico con el nombre del subtopico a trabajar
-subtopico <- "ACECON"
-# 
-# # asignar a outputs
-outputs <- subtopico_init(subtopico_nombre = subtopico, entrega = "primera_entrega")
-
+# # fuentes <- read_csv("data/_FUENTES/fuentes_lista.csv")
+# # 
+# # # asignar var subtopico con el nombre del subtopico a trabajar
+# subtopico <- "ACECON"
+# # 
+# # # asignar a outputs
+# outputs <- subtopico_init(subtopico_nombre = subtopico, entrega = "primera_entrega")
