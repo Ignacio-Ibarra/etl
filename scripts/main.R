c949886ddafcd40bcc4df13a5ba6d5bef4f4ea6e
jgjuara <jg.juara@gmail.com>
2024-03-08 16:52:56 -0300

    limpia main.R

--- scripts/main.R
+++ scripts/main.R
@@ -8,10 +8,3 @@ googledrive::drive_auth(email = Sys.getenv("USER_GMAIL"))
 googlesheets4::gs4_auth(email = Sys.getenv("USER_GMAIL"))
 
 
-# # fuentes <- read_csv("data/_FUENTES/fuentes_lista.csv")
-# # 
-# # # asignar var subtopico con el nombre del subtopico a trabajar
-# subtopico <- "ACECON"
-# # 
-# # # asignar a outputs
-# outputs <- subtopico_init(subtopico_nombre = subtopico, entrega = "primera_entrega")
