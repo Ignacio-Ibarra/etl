9bb9a90841ef5d96597ccfbe591dc2fa258ac30f
jgjuara <jg.juara@gmail.com>
2024-03-18 12:25:12 -0300

    scripts de limpieza de fuentes

--- scripts/limpieza_fuentes/limpieza_cgi_vab_mano_de_obra_indec.R
+++ scripts/limpieza_fuentes/limpieza_cgi_vab_mano_de_obra_indec.R
@@ -1,33 +1,44 @@
-
+# descargar fuente raw desde drive
 descargar_fuente_raw(id_fuente = 35, dir = "data/_FUENTES/raw/")
 
 sheets <- readxl::excel_sheets("data/_FUENTES/raw/serie_cgi_01_24.xls")
 
+# leer los datos
 serie_cgi <- readxl::read_excel("data/_FUENTES/raw/serie_cgi_01_24.xls", sheet = 1)
 
+
+# pivoteo la tabla a long
 serie_cgi <- serie_cgi[-c(1,4:5),] |>
   t() |>
   tibble::as_tibble(.name_repair = "unique")
 
+# asigno nombres de columnas limpios tomando fila 2
 names(serie_cgi) <- serie_cgi[2,] |>
   janitor::make_clean_names()
 
+# quito filas 1:2
 serie_cgi <- serie_cgi[-c(1:2),]
 
+# nombres de cols anio y trim
 serie_cgi <- serie_cgi |>
   dplyr::rename(anio = na, trim = na_2)
 
+# anio a numerico sin marcas adicionale
 serie_cgi <- serie_cgi |>
   dplyr::mutate(anio = as.numeric(gsub(" .*", "", anio )))
 
+# completo filas en blanco con valor de anio correspondiente
 serie_cgi <- serie_cgi |>
   tidyr::fill(anio)
 
+# quito filas en blanco
 serie_cgi <- serie_cgi |>
   dplyr::filter(!is.na(trim))
 
+# quito columnas vacias
 serie_cgi <- serie_cgi[,!sapply(serie_cgi, function(x) {sum(is.na(x)) == length(x)})]
 
+# pivoteo a la long estricto, agrego col unidades y paso valores de millones a unidades
 serie_cgi <- serie_cgi %>% 
   pivot_longer(cols = -c(anio, trim),
                names_to = "indicador", values_to = "valor") %>% 
@@ -35,15 +46,20 @@ serie_cgi <- serie_cgi %>%
           valor = 1E6*as.numeric(valor)) %>% 
   relocate(unidades, .after = trim)
 
+# limpio guiones bajos y numeros de nombres de indicador
 serie_cgi <- serie_cgi %>% 
   mutate(indicador = gsub("_\\d$", "", indicador))
 
+# guardo el df como csv con estilo fundar
 write_csv_fundar(x = serie_cgi, file = "data/_FUENTES/clean/serie_cgi_vab_indec.csv")
 
+# lineas de carga de fuente clean en sheet del drive
+# solo se ejecutan al cargar por primera vez la fuente clean
 
 # agregar_fuente_clean(id_fuente_raw = 35,
 #                      path_clean = "serie_cgi_vab_indec.csv",
 #                      nombre = "CGI - VAB por sector",
 #                      script = "limpieza_cgi_vab_mano_de_obra_indec.R")
 
+# linea de actualizacion para futuras cargas
 actualizar_fuente_clean(id_fuente_clean = 5)
--- scripts/limpieza_fuentes/limpieza_cn_fundacion_norteysur.R
+++ scripts/limpieza_fuentes/limpieza_cn_fundacion_norteysur.R
@@ -1,3 +1,4 @@
+# descargar fuente raw desde drive
 
 descargar_fuente_raw(id_fuente = 36, dir = "data/_FUENTES/raw/")
 
@@ -6,12 +7,12 @@ descargar_fuente_raw(id_fuente = 36, dir = "data/_FUENTES/raw/")
 data <- readxl::read_excel("data/_FUENTES/raw/cuentas-nacionales-fundacion-norte-y-sur.xlsx",
                            sheet = 1, skip = 1)
 
-# rename columns
+# rename anio
 
 data <- data %>% 
   rename(anio  = Año)
 
-# formatear longer
+# armo diccionario de nombres de variables limpios
 diccionario_vars <- data[1:4,-1] %>% 
   select(-where(function(x) all(is.na(x)))) %>% 
   filter(!if_all(everything(), is.na)) %>% 
@@ -19,35 +20,42 @@ diccionario_vars <- data[1:4,-1] %>%
   mutate(name_fixed = str_replace_all(name, "\\.{2}\\d{1,2}", NA_character_)) %>% 
   fill(name_fixed)
 
-# pivot longer
+# pivot longer los datos
 data <- data[-c(1:4),] %>% 
   pivot_longer(cols = -anio,
                names_to = "indicador", values_to = "valor")
 
+# le matcheo los nombres limpios de variabels
 data <- left_join(data, diccionario_vars, by = c("indicador" = "name"))
 
+# excluyo filas vacias
 data <- data %>% 
   filter(!is.na(name_fixed)) %>% 
   select(anio, name_fixed, value, valor)
 
+# agrego columna de pertenencia geografica
 data <- data %>% 
   mutate(iso3 = "ARG")
 
+# seleccion y rename de columnas
 data <- data %>% 
   select(anio, iso3, indicador = name_fixed, unidad = value, valor)
 
+# valores a numerico
 data <- data %>% 
   mutate(valor = as.numeric(valor))
 
+# exlcusion de filas vacias por pivoteo o foramto del excel
 data <- data %>% 
   filter(!is.na(valor))
 
-# guardar
+# guardar como csv
 write_csv_fundar(data,
                  file = "data/_FUENTES/clean/cuentas-nacionales-fundacion-norte-y-sur.csv")  
 
-agregar_fuente_clean(id_fuente_raw = 36,path_clean = "cuentas-nacionales-fundacion-norte-y-sur.csv",
-                     nombre = "Cuentas Nacionales",
-                     script = "limpieza_cn_fundacion_norteysur.R")
+# carga en sheet fuentes clean
+# agregar_fuente_clean(id_fuente_raw = 36,path_clean = "cuentas-nacionales-fundacion-norte-y-sur.csv",
+#                      nombre = "Cuentas Nacionales",
+#                      script = "limpieza_cn_fundacion_norteysur.R")
 
 actualizar_fuente_clean(id_fuente_clean = 4)
