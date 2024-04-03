98629910fa890c1eeedba76bf9beaac86fd68309
jgjuara <jg.juara@gmail.com>
2024-04-03 08:40:10 -0300

    sources de inicio de subtpico

--- scripts/subtopicos/ACECON/0_acecon.R
+++ scripts/subtopicos/ACECON/0_acecon.R
@@ -1,5 +1,5 @@
 subtopico <-  "ACECON"
 analista <-  "andressalles@hotmail.com"
 
-source("scripts/subtopicos/ACECON/0_acecon.R")
+source("scripts/subtopicos/ACECON/fuentes_ACECON.R")
 source("scripts/subtopicos/ACECON/1_pib_pibpc_pob_arg_esp.R")
