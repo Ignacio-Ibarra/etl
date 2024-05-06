6c4ca0178ac9ada65f3eb973e48462e48ec8923c
Ignacio-ibarra <ignacioibarra.data.science@gmail.com>
2024-05-06 10:19:27 -0300

    utilizo estructura de URL para tomar código de indicador

--- scripts/descarga_fuentes/descarga_world_bank_SL_TLF_CACT_FM_NE_ZS.R
+++ scripts/descarga_fuentes/descarga_world_bank_SL_TLF_CACT_FM_NE_ZS.R
@@ -2,29 +2,43 @@ library(sjlabelled)
 
 code_name <- str_split_1(rstudioapi::getSourceEditorContext()$path, pattern = "/") %>% tail(., 1)
 
+url <- "https://data.worldbank.org/indicator/SL.TLF.CACT.FM.NE.ZS"
+indicator_code <- str_split_1(url, "/") %>% tail(.,1)
 
-indicator_code <- 'SL.TLF.CACT.FM.NE.ZS'
 
 # Descargo data usando wrapper https://github.com/vincentarelbundock/WDI
 data <- WDI(indicator=indicator_code, country = 'all')
 
 # Me quedo con el nombre del indicador 
-indicator_label <- get_label(data$SL.TLF.CACT.FM.NE.ZS)
+ind_label <- get_label(data[, c(indicator_code)])
 
-download_filename <- "WDI_ratio_female_to_male_force_participation_rate.csv"
+make_filename <- function(database_abb, indicator_label){
+  indicator_label <- str_to_lower(indicator_label)
+  indicator_label <- str_replace_all(indicator_label, ",", " ")
+  indicator_label <- str_replace_all(indicator_label, " ","_")
+  
+  filename <- sprintf("%s_%s.csv",database_abb, indicator_label)
+  
+  return(filename)
+  
+}
+
+
+database_abb <- "WDI"
+
+download_filename <- make_filename(database_abb, indicator_label = ind_label)
 
 data %>% write_csv_fundar(sprintf("data/_FUENTES/raw/%s",download_filename))
 
-# agregar_fuente_raw(url = "https://data.worldbank.org/indicator/SL.TLF.CACT.FM.NE.ZS",
-#                    nombre = indicator_label, 
+# agregar_fuente_raw(url = url,
+#                    nombre = ind_label,
 #                    institucion = "Banco Mundial",
-#                    actualizable = T, 
+#                    actualizable = T,
 #                    dir = "data/_FUENTES/raw/",
 #                    path_raw = download_filename,
-#                    script = code_name, 
+#                    script = code_name,
 #                    api = T
 # )
 
 actualizar_fuente_raw(id_fuente = 42, actualizable = T, dir = "data/_FUENTES/raw/")
-
 
