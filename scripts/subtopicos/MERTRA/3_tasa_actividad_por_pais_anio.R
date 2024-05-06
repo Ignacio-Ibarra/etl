e0385e6452e7c13086c324d8a0b9580d1306c25f
Ignacio-ibarra <ignacioibarra.data.science@gmail.com>
2024-05-06 13:59:56 -0300

    agrego script de MERTRA tasa_actividad_por_pais_anio.R

--- scripts/subtopicos/MERTRA/3_tasa_actividad_por_pais_anio.R
+++ scripts/subtopicos/MERTRA/3_tasa_actividad_por_pais_anio.R
@@ -1,12 +1,20 @@
 ################################################################################
-##                              Dataset: nombre                               ##
+##                  Dataset: Tasa de Actividad por año y país                 ##
 ################################################################################
 
 #-- Descripcion ----
 #' Breve descripcion de output creado
-#'
 
-output_name <- "nombre del archivo de salida"
+#limpio la memoria
+rm( list=ls() )  #Borro todos los objetos
+gc()   #Garbage Collection
+
+
+subtopico <- "MERTRA"
+output_name <- "tasa_actividad_por_pais_anio"
+fuente1 <- "R45C0"
+fuente2 <- "R46C0"
+
 
 #-- Librerias ----
 
@@ -14,7 +22,9 @@ output_name <- "nombre del archivo de salida"
 
 # Los datos a cargar deben figurar en el script "fuentes_SUBTOP.R" 
 # Se recomienda leer los datos desde tempdir() por ej. para leer maddison database codigo R37C1:
-readr::read_csv(argendataR::get_temp_path("R37C1"))
+df_lab_force <- readr::read_csv(argendataR::get_temp_path(fuente1))
+df_pop <- readr::read_csv(argendataR::get_temp_path(fuente2)) 
+geonomenclador <- argendataR::get_nomenclador_geografico()
 
 
 #-- Parametros Generales ----
@@ -23,7 +33,14 @@ readr::read_csv(argendataR::get_temp_path("R37C1"))
 
 #-- Procesamiento ----
 
-df_outoput <- proceso
+df_lab_force <- df_lab_force %>% select(iso3 = iso3c, anio = year,  lab_force = SL.TLF.TOTL.IN) %>% filter(!is.na(iso3))
+df_pop <- df_pop %>% select(iso3 = iso3c, anio = year,  total_pop = SP.POP.TOTL) %>% filter(!is.na(iso3))
+df <- inner_join(df_lab_force, df_pop) 
+df <- df %>% mutate(tasa_actividad = lab_force / total_pop)
+geonomenclador <- geonomenclador %>% select(codigo_fundar, iso3_desc = desc_fundar, nivel_agregacion)
+df_output <- df %>% select(iso3, anio, tasa_actividad) %>% left_join(., geonomenclador, by=c("iso3"="codigo_fundar")) %>% filter(!is.na(nivel_agregacion))
+
+
 
 #-- Controlar Output ----
 
@@ -47,14 +64,14 @@ df_output %>%
   argendataR::write_output(
     output_name = output_name,
     subtopico = subtopico,
-    fuentes = c("R37C1", "R34C2"),
-    analista = analista,
+    fuentes = c(fuente1, fuente2),
+    analista = "",
     pk = c("anio", "iso3"),
     es_serie_tiempo = T,
     columna_indice_tiempo = "anio",
     columna_geo_referencia = "iso3",
-    nivel_agregacion = "pais",
-    etiquetas_indicadores = list("pbi_per_capita_ppa_porcentaje_argentina" = "PBI per cápita PPA como porcentaje del de Argentina"),
-    unidades = list("pbi_per_capita_ppa_porcentaje_argentina" = "porcentaje")
+    nivel_agregacion = "países y regiones de países",
+    etiquetas_indicadores = list("tasa_actividad" = "Ratio entre la cantidad de personas pertenecientes a la población económicamente activa y la población total"),
+    unidades = list("tasa_actividad" = "porcentaje")
   )
 
