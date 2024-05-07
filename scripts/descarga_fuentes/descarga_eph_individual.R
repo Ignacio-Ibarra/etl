f90a30425e6c251d3d05924f1ff4d2b2ee120aa0
Ignacio-ibarra <ignacioibarra.data.science@gmail.com>
2024-05-07 14:21:48 -0300

    subo script que tienen comenteadas lineas para descargar datos años anteriores, además contiene lineas que descargan año actual si la fecha del momento de ejecución de script es mayor al 05 de Agosto, fecha de publicación del primer cuatrimestre de EPH

--- scripts/descarga_fuentes/descarga_eph_individual.R
+++ scripts/descarga_fuentes/descarga_eph_individual.R
@@ -7,87 +7,84 @@ gc()   #Garbage Collection
 
 code_name <- str_split_1(rstudioapi::getSourceEditorContext()$path, pattern = "/") %>% tail(., 1)
 tipo_encuesta <- "individual"
-version <- paste0("v",format(Sys.Date(), "%Y%m%d"))
+# version <- paste0("v",format(Sys.Date(), "%Y%m%d"))
+version <- "v20240506"
 ext <- "csv"
 output_folder = "data/_FUENTES/raw/"
 
-# Descargar años anteriores
-anio_start <- 2016
-anio_end <- 2023
-
-anio <- 2016
-fuente_id = 63
-for (anio in anio_start:anio_end){
-  download_filename <- glue::glue("eph_{tipo_encuesta}_{anio}_{version}.{ext}")
-  periods <- 1:4
-  
-  if (anio == 2003){
-    periods <- 3:4
-    
-    
-  }
-  
-  if (anio == 2015){
-    periods <- 1:2
-    
-  }
-  
-  if (anio == 2016){
-    periods <- 2:4
-  }
-    
-  df <- eph::get_microdata(year= anio,
-                             period = periods,
-                             type = "individual")  
-  
-  # DT <- data.table(df)
-  # 
-  # # Cita solo los valores que no son numéricos
-  # for (col in names(DT)) {
-  #   if (!is.numeric(DT[[col]])) {
-  #     DT[[col]] <- sprintf('"%s"', DT[[col]])
-  #   }
-  # }
-  # 
-  # # Luego, escribe el data.table en un archivo CSV utilizando fwrite
-  # fwrite(DT, file = "archivo.csv", sep = ",")
-  # 
-  df  %>% write_csv_fundar(., glue::glue("{output_folder}{download_filename}"))
-  
-  
-  # agregar_fuente_raw(url = "https://www.indec.gob.ar/indec/web/Institucional-Indec-BasesDeDatos",
-  #                    nombre = glue::glue("Encuesta Permanente de Hogares, {str_to_title(tipo_encuesta)} ({anio})"),
-  #                    institucion = "INDEC",
-  #                    actualizable = T,
-  #                    dir = output_folder,
-  #                    path_raw = download_filename,
-  #                    script = code_name,
-  #                    api = T
-  # )
-  
-  
-  actualizar_fuente_raw(id_fuente = fuente_id, actualizable = T, dir = "data/_FUENTES/raw/")
-  
-  log <- sprintf("Descargado: id %s año %s archivo %s. \n\n", fuente_id, anio, download_filename)
-  cat(log)
-  
-  fuente_id <- fuente_id + 1
-  }
-
-
 # Descargar última data disponible
-currente_year <- format(Sys.Date, "%Y")
+current_year <- as.numeric(format(Sys.Date(), "%Y"))
+
+# Descargar años anteriores
+# anio_start <- 2003
+# anio_end <- current_year - 1
+periods <- 1:4
+# #Asume que todas las fuentes de EPH se cargaron juntas. 
+# # Toma el id de la EPH 2003 e itera a partir de ahi. 
+# fuente_id = fuentes_raw() %>% 
+#   filter(nombre == "Encuesta Permanente de Hogares, Individual (2003)")  %>% 
+#   select(id_fuente) %>% 
+#   pull()
+# 
+# for (anio in anio_start:anio_end){
+#   download_filename <- glue::glue("eph_{tipo_encuesta}_{anio}_{version}.{ext}")
+#   
+#   if (anio == 2003){
+#     periods <- 3:4
+#     
+#     
+#   }
+#   
+#   if (anio == 2015){
+#     periods <- 1:2
+#     
+#   }
+#   
+#   if (anio == 2016){
+#     periods <- 2:4
+#   }
+#     
+#   df <- eph::get_microdata(year= anio,
+#                              period = periods,
+#                              type = "individual")  
+#   
+#   df  %>% write_csv_fundar(., glue::glue("{output_folder}{download_filename}"))
+#   
+#   
+#   # agregar_fuente_raw(url = "https://www.indec.gob.ar/indec/web/Institucional-Indec-BasesDeDatos",
+#   #                    nombre = glue::glue("Encuesta Permanente de Hogares, {str_to_title(tipo_encuesta)} ({anio})"),
+#   #                    institucion = "INDEC",
+#   #                    actualizable = T,
+#   #                    dir = output_folder,
+#   #                    path_raw = download_filename,
+#   #                    script = code_name,
+#   #                    api = T
+#   # )
+#   
+#   
+#   actualizar_fuente_raw(id_fuente = fuente_id, actualizable = T, dir = "data/_FUENTES/raw")
+#   
+#   log <- sprintf("Actualizado: id %s año %s archivo %s. \n\n", fuente_id, anio, download_filename)
+#   cat(log)
+#   
+#   fuente_id <- fuente_id + 1
+#   }
 
 
+# Descargar current_year
+download_filename <- glue::glue("eph_{tipo_encuesta}_{current_year}_{version}.{ext}")
 
+df <- eph::get_microdata(year= current_year,
+                         period = periods,
+                         type = "individual")
 
+df  %>% write_csv_fundar(., glue::glue("{output_folder}{download_filename}"))
 
-
-
-
-
-
-
-
-
-actualizar_fuente_raw(id_fuente = 49, actualizable = T, dir = "data/_FUENTES/raw/")
+agregar_fuente_raw(url = "https://www.indec.gob.ar/indec/web/Institucional-Indec-BasesDeDatos",
+                  nombre = glue::glue("Encuesta Permanente de Hogares, {str_to_title(tipo_encuesta)} ({current_year})"),
+                  institucion = "INDEC",
+                  actualizable = T,
+                  dir = output_folder,
+                  path_raw = download_filename,
+                  script = code_name,
+                  api = T)
