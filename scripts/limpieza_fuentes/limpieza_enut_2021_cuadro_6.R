37437e7163db0a362d5e0b9ef944185ce7a69986
Ignacio-ibarra <ignacioibarra.data.science@gmail.com>
2024-05-13 15:48:50 -0300

    agrego limpieza de datos de enut 2021 cuadro 6, extraccion y normalizacion de datos en sheet de xlsx

---
+++ scripts/limpieza_fuentes/limpieza_enut_2021_cuadro_6.R
@@ -0,0 +1,83 @@
+# Codigo de limpieza de datos de Encuesta Nacional de Uso del Tiempo 2021 Cuadro 6
+
+#limpio la memoria
+rm( list=ls() )  #Borro todos los objetos
+gc()   #Garbage Collection
+
+limpiar_temps()
+
+
+id_fuente <- 93
+fuente_raw1 <- sprintf("R%sC0",id_fuente)
+
+nombre_archivo_raw <- str_split_1(fuentes_raw() %>% 
+                                    filter(codigo == fuente_raw1) %>% 
+                                    select(path_raw) %>% 
+                                    pull(), pattern = "\\.")[1]
+
+descargar_fuente_raw(id_fuente = id_fuente, tempdir())
+
+sheet <- "Cuadro 6"
+cell_range = "A8:D37"
+
+# Funciona cuando las columnas activas del excel no son más que de la A a la Z
+get_column_names =function(cell_range_str){
+  column_names <- str_split_1(cell_range, "")
+  id1 <- which(LETTERS == column_names[1])
+  id2 <- which(LETTERS == column_names[4])
+  return(LETTERS[id1:id2])
+}
+
+norm_string <- function(string){
+  string <- str_to_lower(string)
+  string <- paste0(string, collapse = "_")
+  return(string)
+  
+}
+
+
+col_names <- get_column_names(cell_range_str = cell_range)
+
+
+enut_df <- readxl::read_excel(argendataR::get_temp_path(fuente_raw1), 
+                              sheet = sheet, 
+                              range = cell_range, 
+                              col_names = col_names,
+                              col_types = "text")
+
+enut_df <- enut_df[(seq(1, nrow(enut_df), by = 2)), ]
+
+
+enut_df_clean <-  enut_df %>% 
+  mutate(sexo = case_when( 
+    A %in% c("Total", "Mujeres", "Varones") ~ A
+  ),
+  nivel_ed = case_when(
+    A %in% c("Total", "Mujeres", "Varones") ~ "Total",
+    TRUE ~ A
+  )
+  ) %>% 
+  fill(sexo) %>% 
+  select(sexo, nivel_ed, `Trabajo total` = B, `En la ocupación` = C, `No remunerado` = D) %>% 
+  pivot_longer(., !any_of(c("sexo", "nivel_ed")), names_to = "tipo_trabajo", values_to = "proporcion_dia") %>% 
+  mutate(proporcion_dia = as.double(replace(proporcion_dia, proporcion_dia == "/// (1)", NA)),
+    minutos_dia = proporcion_dia *24*60)
+
+norm_sheet <- str_to_lower(sheet) %>% str_replace(., " ", "_")
+
+clean_filename <- glue::glue("{norm_sheet}_{nombre_archivo_raw}_CLEAN.csv")
+
+path_clean <- glue::glue("{tempdir()}/{clean_filename}")
+
+enut_df_clean %>% write_csv_fundar(., file = path_clean)
+
+code_name <- str_split_1(rstudioapi::getSourceEditorContext()$path, pattern = "/") %>% tail(., 1)
+
+agregar_fuente_clean(id_fuente_raw = id_fuente,
+                     path_clean = clean_filename,
+                     dir = tempdir(),
+                     nombre = sprintf("Encuesta Nacional de Uso del Tiempo 2021 (%s)",sheet),
+                     script = code_name)
+
+actualizar_fuente_clean(id_fuente_clean = 20,
+                        dir = tempdir())
