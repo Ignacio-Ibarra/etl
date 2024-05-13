45bd1f2c0d4ef6a638a745de5c0acf2167747579
Ignacio-ibarra <ignacioibarra.data.science@gmail.com>
2024-05-13 19:41:39 -0300

    agrego fuentes de MERTRA

--- scripts/subtopicos/MERTRA/fuentes_MERTRA.R
+++ scripts/subtopicos/MERTRA/fuentes_MERTRA.R
@@ -19,7 +19,15 @@ descargar_fuente("R92C15")
 # Encuesta Nacional de Uso del Tiempo 2021 Cuadro 3
 descargar_fuente("R93C17")
 
-# Encuesta Nacional de Uso del Tiempo 2021 Cuadro 3
+# Encuesta Nacional de Uso del Tiempo 2021 Cuadro 4
 descargar_fuente("R93C18")
 
+# Encuesta Nacional de Uso del Tiempo 2021 Cuadro 5
+descargar_fuente("R93C19")
+
+# Encuesta Nacional de Uso del Tiempo 2021 Cuadro 6
+descargar_fuente("R93C20")
+
+# Encuesta Nacional de Uso del Tiempo 2021 Cuadro 14
+descargar_fuente("R93C21")
 
