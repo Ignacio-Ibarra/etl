cb33bb40bb1fa7e92f7dd06ad48602b9e352efbe
TuQmano <juanpabloruiznicolini@gmail.com>
2024-05-20 22:23:43 -0300

    outputs COMEXT
    cantidades expo (indec y ferreres)
    expo bienes y servicios (WDI - banco mundial)

--- scripts/subtopicos/COMEXT/2_exportaciones_bienes_servicios_millones_usd_constantes_2015.R
+++ scripts/subtopicos/COMEXT/2_exportaciones_bienes_servicios_millones_usd_constantes_2015.R
@@ -6,7 +6,7 @@
 #' Breve descripcion de output creado
 #'
 
-output_name <- "nombre del archivo de salida"
+output_name <- "exportaciones_bienes_servicios_millones_usd_constantes_2015"
 
 #-- Librerias ----
 
@@ -14,7 +14,7 @@ output_name <- "nombre del archivo de salida"
 
 # Los datos a cargar deben figurar en el script "fuentes_SUBTOP.R" 
 # Se recomienda leer los datos desde tempdir() por ej. para leer maddison database codigo R37C1:
-readr::read_csv(argendataR::get_temp_path("R37C1"))
+wdi_comext <- readr::read_csv(argendataR::get_temp_path("R98C0"))
 
 
 #-- Parametros Generales ----
@@ -23,7 +23,9 @@ readr::read_csv(argendataR::get_temp_path("R37C1"))
 
 #-- Procesamiento ----
 
-df_outoput <- proceso
+df_output <- wdi_comext %>% 
+  select(time = year, iso3, countryname, exportsconstant_goods_v2, exportsconstant_servi_v2) 
+
 
 #-- Controlar Output ----
 
@@ -34,11 +36,16 @@ df_outoput <- proceso
 comparacion <- argendataR::comparar_outputs(
   df_output,
   nombre = output_name,
-  pk = c("anio", "iso3"),
+  pk = c("time", "iso3"),
   drop_output_drive = F
 )
 
 #-- Exportar Output ----
+
+
+df_output <- df_output %>% 
+  rename(anio = time)
+
 
 # Usar write_output con exportar = T para generar la salida
 # Cambiar los parametros de la siguiente funcion segun su caso
@@ -47,14 +54,14 @@ df_output %>%
   argendataR::write_output(
     output_name = output_name,
     subtopico = subtopico,
-    fuentes = c("R37C1", "R34C2"),
+    fuentes = c("R98C0"),
     analista = analista,
     pk = c("anio", "iso3"),
     es_serie_tiempo = T,
     columna_indice_tiempo = "anio",
     columna_geo_referencia = "iso3",
-    nivel_agregacion = "pais",
-    etiquetas_indicadores = list("pbi_per_capita_ppa_porcentaje_argentina" = "PBI per cápita PPA como porcentaje del de Argentina"),
-    unidades = list("pbi_per_capita_ppa_porcentaje_argentina" = "porcentaje")
+    nivel_agregacion = "pais/region",
+    etiquetas_indicadores = list("exportsconstant_goods_v2" = "Exportaciones de bienes (millones de USD constantes 2015)"),
+    unidades = list("exportsconstant_servi_v2" = "Exportaciones de servicios (millones de USD constantes 2015)")
   )
 
