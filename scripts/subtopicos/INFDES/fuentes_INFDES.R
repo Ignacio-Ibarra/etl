2c0d4190dac60a4fe8e0e3dc155f224586fcf91b
Ignacio-ibarra <ignacioibarra.data.science@gmail.com>
2024-05-29 16:46:10 -0300

    agrego loope usando purrr para que descargue todas las series de EPH disponibles

--- scripts/subtopicos/INFDES/fuentes_INFDES.R
+++ scripts/subtopicos/INFDES/fuentes_INFDES.R
@@ -3,3 +3,7 @@ descargar_fuente("R49C16")
 
 # EPH Total Urbano Diccionario Alomerados, Provincias, Regiones
 descargar_fuente("R84C14")
+
+# EPH descarga de todos los años
+ids.eph <- fuentes_raw() %>% filter(grepl("Encuesta Permanente de Hogares, Individual*", nombre)) %>% select(codigo) %>% pull()
+purrr::map(ids.eph, descargar_fuente)
