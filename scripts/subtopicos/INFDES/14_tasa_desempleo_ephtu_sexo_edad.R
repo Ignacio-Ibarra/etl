dde5558a8caa3dc6125497c73ab81f54c49a34c3
Ignacio-ibarra <ignacioibarra.data.science@gmail.com>
2024-05-29 16:44:42 -0300

    corrijo fuentes input

--- scripts/subtopicos/INFDES/14_tasa_desempleo_ephtu_sexo_edad.R
+++ scripts/subtopicos/INFDES/14_tasa_desempleo_ephtu_sexo_edad.R
@@ -2,28 +2,67 @@
 ##                              Dataset: nombre                               ##
 ################################################################################
 
-#-- Descripcion ----
-#' Breve descripcion de output creado
-#'
+#limpio la memoria
+rm( list=ls() )  #Borro todos los objetos
+gc()   #Garbage Collection
 
-output_name <- "nombre del archivo de salida"
+subtopico <- "INFDES"
+output_name <- "tasa_desempleo_ephtu_sexo_edad"
+fuente1 <- "R49C16"
 
-#-- Librerias ----
+
 
 #-- Lectura de Datos ----
 
 # Los datos a cargar deben figurar en el script "fuentes_SUBTOP.R" 
 # Se recomienda leer los datos desde tempdir() por ej. para leer maddison database codigo R37C1:
-readr::read_csv(argendataR::get_temp_path("R37C1"))
+ephtu_df <- readr::read_csv(argendataR::get_temp_path(fuente1))
 
-
-#-- Parametros Generales ----
-
-# fechas de corte y otras variables que permitan parametrizar la actualizacion de outputs
 
 #-- Procesamiento ----
 
-df_outoput <- proceso
+data <- data.frame(ephtu_df) %>% 
+  select(anio = ano4, sexo = ch04, edad = ch06, estado, pondera) %>% 
+  mutate(
+    desocupado = case_when(
+      estado == 2 ~ 1,
+      estado == 1 ~ 0,
+      TRUE ~ NA
+    ),
+    rango_edad = case_when(
+      edad < 30 ~ 1,
+      edad >= 30 & edad < 40 ~ 2,
+      edad >=40 & edad < 50 ~ 3,
+      edad >= 50 ~ 4
+    ),
+    rango_edad_desc = case_when(
+      edad < 30 ~ "Hasta 30",
+      edad >= 30 & edad < 40 ~ "Entre 30 y 39",
+      edad >=40 & edad < 50 ~ "Entre 40 y 49",
+      edad >= 50 ~ "50 y más"
+    ),
+    sexo = case_when(
+      sexo == 1 ~ "Varones",
+      sexo == 2 ~ "Mujeres",
+      TRUE ~ NA
+    )
+  ) %>% 
+  dplyr::filter(!is.na(desocupado)) %>% 
+  group_by(anio, desocupado, sexo, rango_edad, rango_edad_desc) %>% 
+  summarise(pondera = sum(pondera, na.rm=T)) %>% 
+  ungroup()
+
+
+df_output <- data %>% 
+  group_by(anio, sexo, rango_edad, rango_edad_desc) %>% 
+  mutate(totalactivos = sum(pondera),
+         tasa_desocupacion = pondera / totalactivos) %>%
+  dplyr::filter(desocupado != 0) %>% 
+  select(anio, rango_edad, rango_edad_desc, sexo, tasa_desocupacion)
+  
+
+
+
 
 #-- Controlar Output ----
 
@@ -34,7 +73,7 @@ df_outoput <- proceso
 comparacion <- argendataR::comparar_outputs(
   df_output,
   nombre = output_name,
-  pk = c("anio", "iso3"),
+  pk = c("anio", "rango_edad","sexo"),
   drop_output_drive = F
 )
 
@@ -47,14 +86,11 @@ df_output %>%
   argendataR::write_output(
     output_name = output_name,
     subtopico = subtopico,
-    fuentes = c("R37C1", "R34C2"),
-    analista = analista,
-    pk = c("anio", "iso3"),
+    fuentes = c(fuente1),
+    analista = "",
+    pk = c("anio", "rango_edad","sexo"),
     es_serie_tiempo = T,
     columna_indice_tiempo = "anio",
-    columna_geo_referencia = "iso3",
-    nivel_agregacion = "pais",
-    etiquetas_indicadores = list("pbi_per_capita_ppa_porcentaje_argentina" = "PBI per cápita PPA como porcentaje del de Argentina"),
-    unidades = list("pbi_per_capita_ppa_porcentaje_argentina" = "porcentaje")
+    etiquetas_indicadores = list("tasa_desocupacion" = "Tasa de desocupación"),
+    unidades = list("tasa_desocupacion" = "unidades")
   )
-
