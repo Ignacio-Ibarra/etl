3900ac861fbdf1cd7cfd78a33c37e65b3b1c1bde
Ignacio-ibarra <ignacioibarra.data.science@gmail.com>
2024-05-30 18:25:53 -0300

    agrego fuentes a descargas del subtopico

--- scripts/subtopicos/INFDES/fuentes_INFDES.R
+++ scripts/subtopicos/INFDES/fuentes_INFDES.R
@@ -7,3 +7,16 @@ descargar_fuente("R84C14")
 # EPH descarga de todos los años
 ids.eph <- fuentes_raw() %>% filter(grepl("Encuesta Permanente de Hogares, Individual*", nombre)) %>% select(codigo) %>% pull()
 purrr::map(ids.eph, descargar_fuente)
+
+# Unemployment, total (% of total labor force) (modeled ILO estimate) 
+descargar_fuente("R109C0")
+
+# Unemployment, female (% of female labor force) (modeled ILO estimate)
+descargar_fuente("R110C0")
+
+# Unemployment, male (% of male labor force) (modeled ILO estimate)
+descargar_fuente("R111C0")
+
+# World Values Survey (2017 - 2022)
+descargar_fuente("R105C0")
+
