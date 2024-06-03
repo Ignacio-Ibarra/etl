128bcc9a0b6850df99f959f5948515646c702821
Ignacio-ibarra <ignacioibarra.data.science@gmail.com>
2024-06-03 16:20:47 -0300

    agrego script de limpieza de la hoja 'Labor force' del excel Employment de SEDLAC

--- scripts/limpieza_fuentes/limpieza_cedlas_sedlac_employment_labor_force.R
+++ scripts/limpieza_fuentes/limpieza_cedlas_sedlac_employment_labor_force.R
@@ -16,94 +16,25 @@ nombre_archivo_raw <- str_split_1(fuentes_raw() %>%
                                     pull(), pattern = "\\.")[1]
 
 descargar_fuente_raw(id_fuente = id_fuente, tempdir())
-descargar_fuente("R84C14")
-
-sheets <- tidyxl::xlsx_sheet_names(argendataR::get_temp_path(fuente_raw1)) 
-sheets <- sheets[sheets!="Index"]
 
 # Cargo funciones para hacer limpieza
 source("./scripts/limpieza_fuentes/funciones_limpieza_cedlas_sedlac.R")
 
-# for (sheet in sheets){
-#   
-# }
+TOPIC_PARAM <- "Employment"
+SHEET_PARAM <- "labor force"
 
 
-sheet <- "labor force"
-
-cedlas_df <- readxl::read_excel(argendataR::get_temp_path(fuente_raw1), sheet = sheet, col_names = F) %>%
+cedlas_df <- readxl::read_excel(argendataR::get_temp_path(fuente_raw1), sheet = SHEET_PARAM, col_names = F) %>%
   select_if(~ !all(is.na(.))) %>%
   filter(rowSums(is.na(.)) < ncol(.)) %>%
   mutate(across(everything(), as.character)) %>%
   as.data.frame()
 
+tematica <- cedlas_df[[1,1]]
+variable <- cedlas_df[[2,1]]
 
-columnas_sheet <- obtengo_columnas(df = cedlas_df)
-
-tabla_numeros <- obtengo_tabla_numeros(df = cedlas_df)
-
-df <- data.frame(cedlas_df)
-
-
-obtengo_paises_fuentes <- function(df, lista.paises) {
-  df <- df %>%
-    select(where(~ !all(is.na(.)))) %>%
-    filter(rowSums(is.na(.)) != ncol(.)) %>%
-    as.data.frame()
-  
-  locs <- cols_and_data_row_locations(df)
-  start_data <- locs$start_data
-  
-  col0 <- df[[1]]
-  strings <- lapply(col0[(start_data-2):length(col0)], function(x) strip_chars(x) ) %>% unlist()
-  
- 
-   paises_fuentes_dates <- data.frame(
-    pais = character(),
-    fuente = character(),
-    fuente_orden = integer(),
-    encuesta_date = character(),
-    stringsAsFactors = FALSE
-  )
-  
-  fuente_orden <- 0
-  
-  for (v in strings) {
-    cat(v)
-    if (!es_entero(v)) {
-      if (v %in% lista_paises) {
-        pais <- v
-        fuente <- NULL
-        fuente_orden <- 0
-      } 
-      else {
-        if (is.na(as.numeric(strsplit(v, "-")[[1]][1]))) {
-          fuente <- v
-          fuente_orden <- fuente_orden + 1
-        } 
-        else {
-          encuesta_date <- v
-          # paises_fuentes_dates <- append(paises_fuentes_dates, list(list(pais, fuente, fuente_orden, encuesta_date)))
-        }
-      }
-    } 
-    else {
-      encuesta_date <- v
-      # paises_fuentes_dates <- append(paises_fuentes_dates, list(list(pais, fuente, fuente_orden, encuesta_date)))
-    }
-    paises_fuentes_dates <- rbind(paises_fuentes_dates,
-      data.frame(pais = pais, fuente = fuente, fuente_orden = fuente_orden, encuesta_date = encuesta_date, stringsAsFactors = FALSE))
-  }
-}  
-
-
-
-
-lista_paises <- c("Argentina", "Bolivia", "Brazil", "Chile", "Colombia",
-                  "Costa Rica", "Dominican Rep", "Ecuador", "El Salvador",
-                  "Guatemala", "Honduras", "Mexico", "Nicaragua", "Panama",
-                  "Paraguay", "Peru", "Uruguay", "Venezuela")
-
+geonomenclador <- argendataR::get_nomenclador_geografico()
+mapeo_iso3_pais <- setNames(geonomenclador$desc_fundar, geonomenclador$codigo_fundar)
 
 mapeo_pais_iso3_adhoc <- c(
   'Argentina' = 'ARG', 
@@ -126,5 +57,34 @@ mapeo_pais_iso3_adhoc <- c(
   'Venezuela' = 'VEN'
 )
 
+lista_paises <- names(mapeo_pais_iso3_adhoc)
 
+cedlas_clean <- armar_tabla(df = cedlas_df, 
+                            topico = TOPIC_PARAM,
+                            tematica = tematica,
+                            variable = variable,
+                            lista.paises = lista_paises, 
+                            mapper.paises_cedlas.a.isos = mapeo_pais_iso3_adhoc,
+                            mapper.isos.a.paises = mapeo_iso3_pais) 
+
+
+
+norm_sheet <- str_to_lower(SHEET_PARAM) %>% str_replace(., " ", "_")
+
+clean_filename <- glue::glue("{norm_sheet}_{nombre_archivo_raw}_CLEAN.csv")
+
+path_clean <- glue::glue("{tempdir()}/{clean_filename}")
+
+cedlas_clean %>% write_csv_fundar(., file = path_clean)
+
+code_name <- str_split_1(rstudioapi::getSourceEditorContext()$path, pattern = "/") %>% tail(., 1)
+
+# agregar_fuente_clean(id_fuente_raw = id_fuente,
+#                      path_clean = clean_filename,
+#                      dir = tempdir(),
+#                      nombre = glue::glue("{TOPIC_PARAM} - {tematica} - SEDLAC"),
+#                      script = code_name)
+
+actualizar_fuente_clean(id_fuente_clean = 30,
+                        dir = tempdir())
 
