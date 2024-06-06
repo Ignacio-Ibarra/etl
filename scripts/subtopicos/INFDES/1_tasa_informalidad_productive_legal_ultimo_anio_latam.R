7b6911f928028bcf84f3a23f393cdb93fccbc305
Ignacio-ibarra <ignacioibarra.data.science@gmail.com>
2024-06-06 16:25:14 -0300

    agrego script de comparativa entre la tasa de informalidad legal y productiva para algunos países de Latam, datos del ultimo anio disponible, fuente SEDLAC

--- scripts/subtopicos/INFDES/1_tasa_informalidad_productive_legal_ultimo_anio_latam.R
+++ scripts/subtopicos/INFDES/1_tasa_informalidad_productive_legal_ultimo_anio_latam.R
@@ -2,28 +2,42 @@
 ##                              Dataset: nombre                               ##
 ################################################################################
 
-#-- Descripcion ----
-#' Breve descripcion de output creado
-#'
+#limpio la memoria
+rm( list=ls() )  #Borro todos los objetos
+gc()   #Garbage Collection
 
-output_name <- "nombre del archivo de salida"
+subtopico <- "INFDES"
+output_name <- "tasa_informalidad_productive_legal_ultimo_anio_latam"
+fuente1 <- "R115C31"
+fuente2 <- "R115C32"
 
-#-- Librerias ----
 
 #-- Lectura de Datos ----
 
 # Los datos a cargar deben figurar en el script "fuentes_SUBTOP.R" 
 # Se recomienda leer los datos desde tempdir() por ej. para leer maddison database codigo R37C1:
-readr::read_csv(argendataR::get_temp_path("R37C1"))
-
-
-#-- Parametros Generales ----
-
-# fechas de corte y otras variables que permitan parametrizar la actualizacion de outputs
+inf_prod_def <- readr::read_csv(argendataR::get_temp_path(fuente1))
+inf_legal_def <- readr::read_csv(argendataR::get_temp_path(fuente2))
 
 #-- Procesamiento ----
 
-df_outoput <- proceso
+informality_df <- bind_rows(inf_prod_def, inf_legal_def) %>% dplyr::filter((apertura == "Edad total") & (serie == "Serie original"))
+
+informality_df <- informality_df %>% 
+  mutate(tematica = ifelse(tematica == "Informalidad", "Informalidad (definición legal)", tematica),
+         variable = str_replace(variable,"Proporción", "Participación"))
+
+informality_df <- informality_df %>% dplyr::filter(anio>2015)
+
+df_output <- informality_df %>% 
+  group_by(pais, tematica) %>% 
+  mutate(dummy_anio_max = max(anio) == anio) %>% 
+  ungroup() %>% 
+  filter(dummy_anio_max) %>% 
+  select(iso3, pais, anio, tipo_informalidad = tematica, valor)
+
+
+ 
 
 #-- Controlar Output ----
 
@@ -34,8 +48,8 @@ df_outoput <- proceso
 comparacion <- argendataR::comparar_outputs(
   df_output,
   nombre = output_name,
-  pk = c("anio", "iso3"),
-  drop_output_drive = F
+  pk = c("iso3", "anio","tipo_informalidad"),
+  drop_joined_df = F
 )
 
 #-- Exportar Output ----
@@ -47,14 +61,14 @@ df_output %>%
   argendataR::write_output(
     output_name = output_name,
     subtopico = subtopico,
-    fuentes = c("R37C1", "R34C2"),
-    analista = analista,
-    pk = c("anio", "iso3"),
+    fuentes = c(fuente1, fuente2),
+    analista = "",
+    pk = c("iso3", "anio","tipo_informalidad"),
     es_serie_tiempo = T,
     columna_indice_tiempo = "anio",
     columna_geo_referencia = "iso3",
     nivel_agregacion = "pais",
-    etiquetas_indicadores = list("pbi_per_capita_ppa_porcentaje_argentina" = "PBI per cápita PPA como porcentaje del de Argentina"),
-    unidades = list("pbi_per_capita_ppa_porcentaje_argentina" = "porcentaje")
+    etiquetas_indicadores = list("valor" = "Tasa de informalidad"),
+    unidades = list("valor" = "porcentaje")
   )
 
