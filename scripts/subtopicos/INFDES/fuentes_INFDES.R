5d75d1f8301e99c564a7a0c8fd17949738647793
Ignacio-ibarra <ignacioibarra.data.science@gmail.com>
2024-06-10 10:40:56 -0300

    agrego descargas de fuentes al subtopico INFDES

--- scripts/subtopicos/INFDES/fuentes_INFDES.R
+++ scripts/subtopicos/INFDES/fuentes_INFDES.R
@@ -20,3 +20,14 @@ descargar_fuente("R111C0")
 # World Values Survey (2017 - 2022)
 descargar_fuente("R105C0")
 
+# Employment - Informality (productive definition) - SEDLAC (serie original y empalmada)
+descargar_fuente("R115C31")
+
+# Employment - Informality - SEDLAC (serie original y empalmada)
+descargar_fuente("R115C32")
+
+# Employment - Employment structure 3 - SEDLAC (serie original y empalmada)
+descargar_fuente("R115C39")
+
+# GDP per capita, PPP (constant 2021 international $)
+descargar_fuente("R126C0")
