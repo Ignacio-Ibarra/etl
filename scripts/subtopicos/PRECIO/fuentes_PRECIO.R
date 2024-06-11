bd4b26a75ab416996def57427f23cb608a3cdfb9
jgjuara <jg.juara@gmail.com>
2024-06-11 17:40:11 -0300

    fuentes precio

--- scripts/subtopicos/PRECIO/fuentes_PRECIO.R
+++ scripts/subtopicos/PRECIO/fuentes_PRECIO.R
@@ -3,6 +3,22 @@
 ################################################################################
 
 
-#-- Fuente 1: nombre de la fuente / Codigo de fuente: RXXCX
-descargar_fuente("RXXCX")
+#-- Fuente 1: Ponderadores de divisiones del IPC por región y para total nacional. Año 2016
+descargar_fuente("R117C29")
+
+
+#-- Fuente 2: Indice y variacion de precios 1809 a 2018 FNyS -------------------------------------------------------------------------
+descargar_fuente("R120C49")
+
+#-- Fuente 3: Indice de precios al consumidor (IPC) - San Luis -------------------------------------------------------------------------
+descargar_fuente("R124C50")
+
+#-- Fuente 4: Indice de precios al consumidor (IPC) por regiones según divisiones INDEC -------------------------------------------------------------------------
+descargar_fuente("R127C54")
+
+#-- Fuente 5: World Economic Outlook FMI -------------------------------------------------------------------------
+descargar_fuente("R34C2")
+
+#-- Fuente 6: Indice de precios al consumidor (IPC) GBA serie historica INDEC -------------------------------------------------------------------------
+descargar_fuente("R128C53")
 
