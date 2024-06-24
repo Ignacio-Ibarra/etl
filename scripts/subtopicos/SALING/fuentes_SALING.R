c58c10ef7948f07a8bf2b38118014dc00f9dbaa1
Ignacio-ibarra <ignacioibarra.data.science@gmail.com>
2024-06-24 13:19:32 -0300

    generacion automatica de scripts de SALING fuente ISA CEDLAS

---
+++ scripts/subtopicos/SALING/fuentes_SALING.R
@@ -0,0 +1,161 @@
+# Indicadores Sociales de Argentina: Participación del ingreso laboral en el ingreso total familiar, 2003-2023.
+
+
+descargar_fuente('R163C0')
+
+
+
+# Indicadores Sociales de Argentina: Participación de las distintas fuentes en el ingreso familiar total. 2023, Semestre I. Por deciles del ingreso per cápita familiar.
+
+
+descargar_fuente('R164C0')
+
+
+
+# Indicadores Sociales de Argentina: Participación de las distintas fuentes en el ingreso familiar total. 2023, Semestre I. Por edad del jefe de hogar.
+
+
+descargar_fuente('R165C0')
+
+
+
+# Indicadores Sociales de Argentina: Participación del ingreso laboral en el ingreso total familiar, 2003-2023. Según género del jefe/a del hogar
+
+
+descargar_fuente('R166C0')
+
+
+
+# Indicadores Sociales de Argentina: Evolución de las distintas fuentes de ingresos no laborales, 2003-2023 (2003=100).
+
+
+descargar_fuente('R167C0')
+
+
+
+# Indicadores Sociales de Argentina: Perfil de ingresos laborales mensuales por edad y género
+
+
+descargar_fuente('R168C0')
+
+
+
+# Indicadores Sociales de Argentina: Perfil de ingresos laborales por hora en la ocupación principal por edad y género
+
+
+descargar_fuente('R169C0')
+
+
+
+# Indicadores Sociales de Argentina: Evolución de salario real por grupos etarios y género en términos reales
+
+
+descargar_fuente('R170C0')
+
+
+
+# Indicadores Sociales de Argentina: Ingreso familiar per cápita mensual en dólares a PPA (2017), último dato disponible.
+
+
+descargar_fuente('R171C0')
+
+
+
+# Indicadores Sociales de Argentina: Ingreso familiar per cápita mensual en dólares a PPA (2017), 1992-2021.
+
+
+descargar_fuente('R172C0')
+
+
+
+# Indicadores Sociales de Argentina: Ingreso familiar per cápita, 2003-2023.
+
+
+descargar_fuente('R173C0')
+
+
+
+# Indicadores Sociales de Argentina: Ingreso per cápita familiar relativo al promedio nacional, 2023.
+
+
+descargar_fuente('R174C0')
+
+
+
+# Indicadores Sociales de Argentina: Evolución del ingreso per cápita familiar, 2003-2023.
+
+
+descargar_fuente('R175C0')
+
+
+
+# Indicadores Sociales de Argentina: Ingreso laboral horario e ingreso laboral mensual en términos reales
+
+
+descargar_fuente('R176C0')
+
+
+
+# Indicadores Sociales de Argentina: Indice de Salarios de INDEC a precios constantes
+
+
+descargar_fuente('R177C0')
+
+
+
+# Indicadores Sociales de Argentina: Salario real medio
+
+
+descargar_fuente('R178C0')
+
+
+
+# Indicadores Sociales de Argentina: Ingreso laboral horario
+
+
+descargar_fuente('R179C0')
+
+
+
+# Indicadores Sociales de Argentina: Ingreso laboral horario
+
+
+descargar_fuente('R180C0')
+
+
+
+# Indicadores Sociales de Argentina: Ingresos laborales de cada región por tipo de calificación del trabajador
+
+
+descargar_fuente('R181C0')
+
+
+
+# Indicadores Sociales de Argentina: Evolución de los ingresos laborales reales medios por región
+
+
+descargar_fuente('R182C0')
+
+
+
+# Indicadores Sociales de Argentina: Ingresos laborales a precios constantes por tipo de empleo
+
+
+descargar_fuente('R183C0')
+
+
+
+# Indicadores Sociales de Argentina: Índice de salarios de INDEC a precios constantes por tipo de empleo
+
+
+descargar_fuente('R184C0')
+
+
+
+# Indicadores Sociales de Argentina: Ingresos laborales mensuales por tipo de trabajo (en dólares a PPA)
+
+
+descargar_fuente('R185C0')
+
+
+
