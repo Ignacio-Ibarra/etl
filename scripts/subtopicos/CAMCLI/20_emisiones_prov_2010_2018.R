eb6c3baf48611777e93374e9dd53937ded8a6d81
JotaEmeDi <juamadias@gmail.com>
2024-06-24 13:34:03 -0300

    subo ultimo scripting camcli

--- scripts/subtopicos/CAMCLI/20_emisiones_prov_2010_2018.R
+++ scripts/subtopicos/CAMCLI/20_emisiones_prov_2010_2018.R
@@ -7,7 +7,7 @@
 #'
 #'
 
-output_name <- "emisiones_residuos_1990_2018"
+output_name <- "emisiones_prov_2010_2018"
 
 #-- Librerias ----
 
@@ -16,11 +16,12 @@ output_name <- "emisiones_residuos_1990_2018"
 # Los datos a cargar deben figurar en el script "fuentes_SUBTOP.R" 
 # Se recomienda leer los datos desde tempdir() por ej. para leer maddison database codigo R37C1:
 
-descargar_fuente_raw(id_fuente = 131, tempdir())
+descargar_fuente_raw(id_fuente = 157, tempdir())
 
 # traigo la data 
-emis_1990_2018_arg_residuos<- readxl::read_xlsx (argendataR::get_temp_path("R131C0"),skip = 1) %>% 
-  clean_names()
+
+emisiones_provincias_2010_2018<- readr::read_csv(argendataR::get_temp_path("R157C67")) %>% 
+  mutate(provincia = ifelse(provincia == "PBA", "Buenos Aires", provincia)) # le cambio PBA que no lo hice en limpieza
 
 #-- Parametros Generales ----
 
@@ -28,27 +29,12 @@ emis_1990_2018_arg_residuos<- readxl::read_xlsx (argendataR::get_temp_path("R131
 
 #-- Procesamiento ----
 
-emis_1990_2018_arg_residuos_final <- emis_1990_2018_arg_residuos %>%
-  filter(sector == "Residuos") %>%
-  mutate(sector = "Residuos") %>% 
-  mutate(subsector = case_when(
-    categoria %in% c("Eliminación de residuos sólidos",
-                     "Incineración de residuos",
-                     "Tratamiento biológico de los Residuos sólidos") ~ "Residuos sólidos",
-    categoria=="Tratamiento y eliminación de aguas residuales" ~ "Aguas residuales",
-    TRUE ~ NA_character_)) %>%
-  group_by(ano, sector, subsector) %>%
-  summarise(valor_en_mtco2e = round(sum(valor, na.rm = TRUE), 2)) %>% 
-  rename(anio=ano) %>%
-  drop_na() 
-
 #-- Controlar Output ----
 
 # Usar la funcion comparar_outputs para contrastar los cambios contra la version cargada en el Drive
 # Cambiar los parametros de la siguiente funcion segun su caso
 
-
-df <- emis_1990_2018_arg_residuos_final
+df <- emisiones_provincias_2010_2018
 
 df_anterior <- descargar_output(nombre=output_name,
                                 subtopico = "CAMCLI",
@@ -75,11 +61,11 @@ comparacion <- argendataR::comparar_outputs(df,
 
 df_output %>%
   argendataR::write_output(
-    output_name = "emisiones_residuos_1990_2018",
+    output_name = "emisiones_prov_2010_2018",
     subtopico = "CAMCLI",
-    fuentes = c("R131C0"),
+    fuentes = c("R157C67"),
     analista = "",
-    pk = c("anio","sector","subsector"),
+    pk = c("anio","sector","provincia"),
     es_serie_tiempo = F,
     columna_indice_tiempo = "anio",
     #columna_geo_referencia = "iso3",
--- scripts/subtopicos/CAMCLI/3_01_evolucion_CO2_historico.R
+++ scripts/subtopicos/CAMCLI/3_01_evolucion_CO2_historico.R
@@ -13,29 +13,47 @@ output_name <- "01_evolucion_CO2_historico"
 #-- Lectura de Datos ----
 # Los datos a cargar deben figurar en el script "fuentes_SUBTOP.R" 
 # Se recomienda leer los datos desde tempdir() por ej. para leer maddison database codigo R37C1:
+
+# traigo data de años ára atrás al presente 137 al 800.000 aprox y le sumo data de 1958 a 2024
+
 co2_hist<-read_csv(argendataR::get_temp_path("R100C26"))
+co2_1958_2024<-read_csv(argendataR::get_temp_path("R161C71"))
 
-#-- Parametros Generales ----
+#-- Procesamiento ----
 
-# fechas de corte y otras variables que permitan parametrizar la actualizacion de outputs
+# Creo nuevo dataframe con las columnas "fecha_estimada" y "co2_ppm" del segundo dataset
+co2_1958_2024_nuevo <- co2_1958_2024 %>%
+  select(anio, CO2_ppmv_deseasonalized) %>%
+  rename(fecha_estimada = anio, co2_ppm = CO2_ppmv_deseasonalized)
+
+# junto con co2_hist
+
+co2_hist_y_1958_2024 <- co2_hist %>%
+  bind_rows(co2_1958_2024_nuevo)
 
 #-- Controlar Output ----
-
-df_output <- co2_hist
 
 # Usar la funcion comparar_outputs para contrastar los cambios contra la version cargada en el Drive
 # Cambiar los parametros de la siguiente funcion segun su caso
 
-comparacion <- argendataR::comparar_outputs(
-  co2_hist,
-  subtopico = "CAMCLI",
-  entrega_subtopico = "segunda_entrega",
-  nombre = output_name,
-  k_control_num = 3,
-  pk = c("fecha_estimada"),
-  drop_joined_df = F
-)
+df <- co2_hist_y_1958_2024
 
+df_anterior <- descargar_output(nombre=output_name,
+                                subtopico = "CAMCLI",
+                                entrega_subtopico = "datasets_segunda_entrega")
+
+#-- Controlar Output ----
+
+# Usar la funcion comparar_outputs para contrastar los cambios contra la version cargada en el Drive
+# Cambiar los parametros de la siguiente funcion segun su caso
+
+df_output <- df
+
+comparacion <- argendataR::comparar_outputs(df,
+                                            df_anterior,
+                                            k_control_num = 3,
+                                            pk = c("fecha_estimada"),
+                                            drop_joined_df = F)
 
 #-- Exportar Output ----
 
@@ -44,19 +62,17 @@ comparacion <- argendataR::comparar_outputs(
 
 df_output %>%
   argendataR::write_output(
-    output_name = output_name,
+    output_name = "concentración c02 histórica",
     subtopico = "CAMCLI",
-    fuentes = c("R100C26"),
+    fuentes = c("R100C26","R161C71"),
     analista = "",
     pk = c("fecha_estimada"),
     es_serie_tiempo = T,
     columna_indice_tiempo = "fecha_estimada",
-#    columna_geo_referencia = "",
-#    nivel_agregacion = "pais",
-    etiquetas_indicadores = list("co2_ppm" = "Concentración co2 atmósfera"),
-    unidades = list("co2_ppm" = "partes por millón (ppm)"),
-    directorio = "data/CAMCLI/"
+    aclaraciones = "luego de darles vueltas al tema este gráfico se dibuja por diseño. pero de todas formas a co2_hist que va de -137 a -900 mil le trajimos la data de 1958 a 2024",
+    #columna_geo_referencia = "iso3",
+    nivel_agregacion = "global",
+    etiquetas_indicadores = list("fecha_estimada" = "Fecha estimada","co2_ppm"="Evolución histórica de CO2"),
+    unidades = list("co2_ppm" = "Partes por millón")
   )
 
-
-list.files(tempdir())
