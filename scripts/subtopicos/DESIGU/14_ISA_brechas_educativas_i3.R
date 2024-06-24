6cb4129744f114b5918bab9caa66e625408cd00d
Ignacio-ibarra <ignacioibarra.data.science@gmail.com>
2024-06-24 13:57:59 -0300

    agrego scripts generados automaticamente

---
+++ scripts/subtopicos/DESIGU/14_ISA_brechas_educativas_i3.R
@@ -0,0 +1,45 @@
+#limpio la memoria
+rm( list=ls() )  #Borro todos los objetos
+gc()   #Garbage Collection
+
+limpiar_temps()
+
+code_name <- '14_ISA_brechas_educativas_i3.R'
+subtopico <- 'DESIGU'
+output_name <- 'ISA_brechas_educativas_i3.csv'
+id_fuente <- 188
+fuente_raw1 <- sprintf("R%sC0",id_fuente)
+
+nombre_archivo_raw <- str_split_1(fuentes_raw() %>% 
+                                    filter(codigo == fuente_raw1) %>% 
+                                    select(path_raw) %>% 
+                                    pull(), pattern = "\\.")[1]
+
+df_output <- readxl::read_excel(argendataR::get_temp_path(fuente_raw1)) 
+
+df_anterior <- argendataR::descargar_output(nombre = output_name, subtopico = subtopico, entrega_subtopico = "primera_entrega")
+
+#-- Controlar Output ----
+
+comparacion <- argendataR::comparar_outputs(
+  df_output,
+  df_anterior,
+  pk = c('ano','variable'),
+  drop_joined_df = F
+)
+
+
+df_output %>%
+  argendataR::write_output(
+    output_name = output_name,
+    subtopico = subtopico,
+    fuentes = c(fuente_raw1),
+    analista = "",
+    pk =  c('ano','variable'),
+    es_serie_tiempo = [DEFINIR],
+    columna_indice_tiempo = [DEFINIR],
+    nivel_agregacion =[DEFINIR],
+    aclaraciones = [DEFINIR],
+    etiquetas_indicadores = list([DEFINIR] = [DEFINIR]),
+    unidades = list([DEFINIR] = [DEFINIR])
+  )
