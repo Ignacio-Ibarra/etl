170e81e954c75ea19669b095c3da6dafa4ec8a16
Ignacio-ibarra <ignacioibarra.data.science@gmail.com>
2024-06-25 14:45:22 -0300

    actualizo script de descarga de cuenta generacion ingreso para poder descargar nueva data

--- scripts/descarga_fuentes/descarga_cuenta_generacion_ingreso_indec.R
+++ scripts/descarga_fuentes/descarga_cuenta_generacion_ingreso_indec.R
@@ -1,15 +1,15 @@
-serie_cgi <- "https://www.indec.gob.ar/ftp/cuadros/economia/serie_cgi_01_24.xls"
+serie_cgi <- "https://www.indec.gob.ar/ftp/cuadros/economia/serie_cgi_04_24.xls"
 
 download.file(serie_cgi,
               mode = "wb", # archivos tipo xlsx requieren escritura tipo binaria
-              destfile = glue::glue("data/_FUENTES/raw/serie_cgi.xls"))
+              destfile = glue::glue("{tempdir()}/serie_cgi.xls"))
 
 
-agregar_fuente_raw(url = serie_cgi,institucion = "INDEC", actualizable = T,
-               fecha_descarga = Sys.Date(),
-               path_raw = "serie_cgi.xls",
-               script = "descarga_cuenta_generacion_ingreso_indec.R",
-               nombre = "Valor agregado bruto e insumo de mano de obra por sector de actividad económica"
-                )
+# agregar_fuente_raw(url = serie_cgi,institucion = "INDEC", actualizable = T,
+#                fecha_descarga = Sys.Date(),
+#                path_raw = "serie_cgi.xls",
+#                script = "descarga_cuenta_generacion_ingreso_indec.R",
+#                nombre = "Valor agregado bruto e insumo de mano de obra por sector de actividad económica"
+#                 )
 
-actualizar_fuente(id_fuente = 35, fecha_descarga = Sys.Date())
+actualizar_fuente_raw(id_fuente = 35, fecha_descarga = Sys.Date(), path_raw = "serie_cgi.xls" )
