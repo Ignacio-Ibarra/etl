bcedce74c3c7720aac20db7edf5e471bcbb0f77c
Ignacio-ibarra <ignacioibarra.data.science@gmail.com>
2024-07-10 13:48:28 -0300

    modifico codigo 0 y corrijo calculo en codigo 14

--- scripts/subtopicos/COMEXT/14_composicion_exportaciones_bienes_diferenciado_Bernini.R
+++ scripts/subtopicos/COMEXT/14_composicion_exportaciones_bienes_diferenciado_Bernini.R
@@ -5,7 +5,7 @@
 #-- Descripcion ----
 #' Breve descripcion de output creado
 #'
-
+subtopico <- "COMEXT"
 
 code_name <- str_split_1(rstudioapi::getSourceEditorContext()$path, pattern = "/") %>% tail(., 1)
 
@@ -30,7 +30,7 @@ comex_baci_diferenciados_berinini <- readr::read_csv(argendataR::get_temp_path("
 #-- Procesamiento ----
 
 df_output <- comex_baci_diferenciados_berinini %>% 
-  dplyr::filter(!is.na(microd)) %>% 
+  # dplyr::filter(!is.na(microd)) %>% 
   dplyr::mutate(microd_name = dplyr::case_when(
     microd == "D" ~ 'Diferenciado', TRUE ~ 'No diferenciado'), 
                 microd = dplyr::case_when(
@@ -50,7 +50,7 @@ df_anterior <- df_anterior %>%
 
 
 comparacion <- argendataR::comparar_outputs(df = df_output, df_anterior = df_anterior,
-                                            pk = c("year", "iso3", 'country_name_abbreviation', 'microd', 'microd_name'))
+                                            pk = c("year", "iso3", 'microd', 'microd_name'))
 
 
 #-- Exportar Output ----
@@ -61,14 +61,14 @@ comparacion <- argendataR::comparar_outputs(df = df_output, df_anterior = df_ant
 df_output %>%
   argendataR::write_output(
     control = comparacion, 
-    directorio = 'data/COMEXT/',
+    directorio = tempdir(),
     output_name = output_name,
     subtopico = subtopico,
     fuentes = c("R113C57"),
-    analista = analista,
+    analista = "",
     pk = c("year", "iso3", 'country_name_abbreviation', 'microd', 'microd_name'),
     es_serie_tiempo = FALSE,
-    columna_indice_tiempo = "anio",
+    columna_indice_tiempo = "year",
     columna_geo_referencia = "iso3",
     nivel_agregacion = "pais",
     etiquetas_indicadores = list("export_value_pc" = "Exportaciones de bienes (% del total exportado en bienes)"),
