c202bda3f315772b2cea9b104ce42039529dcd8a
Ignacio-ibarra <ignacioibarra.data.science@gmail.com>
2024-10-04 18:39:40 -0300

    agrego scripts de outputs

---
+++ scripts/subtopicos/ESTPRO/3_va_sectorial_pais_bys.R
@@ -0,0 +1,127 @@
+################################################################################
+##                              Dataset: nombre                               ##
+################################################################################
+
+#limpio la memoria
+rm( list=ls() )  #Borro todos los objetos
+gc()   #Garbage Collection
+
+
+subtopico <- "ESTPRO"
+output_name <- "va_sectorial_pais_bys"
+analista = "Gisella Pascuariello"
+fuente1 <- "R224C95"
+
+
+
+get_raw_path <- function(codigo){
+  prefix <- glue::glue("{Sys.getenv('RUTA_FUENTES')}raw/")
+  df_fuentes_raw <- fuentes_raw() 
+  path_raw <- df_fuentes_raw[df_fuentes_raw$codigo == codigo,c("path_raw")]
+  return(paste0(prefix, path_raw))
+}
+
+get_clean_path <- function(codigo){
+  prefix <- glue::glue("{Sys.getenv('RUTA_FUENTES')}clean/")
+  df_fuentes_clean <- fuentes_clean() 
+  path_clean <- df_fuentes_clean[df_fuentes_clean$codigo == codigo,c("path_clean")]
+  return(paste0(prefix, path_clean))
+}
+
+
+# Cargo data desde server
+df_output <- arrow::read_parquet(get_clean_path(fuente1)) %>% 
+  dplyr::filter(tipo_sector == "Servicios") %>% 
+  group_by(anio, iso3, pais_nombre) %>% 
+  summarise(particip_servicios_pib = sum(share_vab)/100)
+  
+
+df_anterior <- argendataR::descargar_output(nombre = output_name, subtopico = subtopico, entrega_subtopico = "primera_entrega") %>% 
+  dplyr::filter(!(is.na(particip_servicios_pib)))
+
+
+comparacion <- argendataR::comparar_outputs(
+  df_anterior = df_anterior,
+  df = df_output,
+  nombre = output_name,
+  pk = c("anio","iso3"), # variables pk del dataset para hacer el join entre bases
+  drop_joined_df =  F
+)
+
+
+#-- Exportar Output ----
+
+armador_descripcion <- function(metadatos, etiquetas_nuevas = data.frame(), output_cols){
+  # metadatos: data.frame sus columnas son variable_nombre y descripcion y 
+  # proviene de la info declarada por el analista 
+  # etiquetas_nuevas: data.frame, tiene que ser una dataframe con la columna 
+  # variable_nombre y la descripcion
+  # output_cols: vector, tiene las columnas del dataset que se quiere escribir
+  
+  etiquetas <- metadatos %>% 
+    dplyr::filter(variable_nombre %in% output_cols) 
+  
+  
+  etiquetas <- etiquetas %>% 
+    bind_rows(etiquetas_nuevas)
+  
+  
+  diff <- setdiff(output_cols, etiquetas$variable_nombre)
+  
+  stopifnot(`Error: algunas columnas de tu output no fueron descriptas` = length(diff) == 0)
+  
+  # En caso de que haya alguna variable que le haya cambiado la descripcion pero que
+  # ya existia se va a quedar con la descripcion nueva. 
+  
+  etiquetas <- etiquetas %>% 
+    group_by(variable_nombre) %>% 
+    filter(if(n() == 1) row_number() == 1 else row_number() == n()) %>%
+    ungroup()
+  
+  etiquetas <- stats::setNames(as.list(etiquetas$descripcion), etiquetas$variable_nombre)
+  
+  return(etiquetas)
+  
+}
+
+# Tomo las variables output_name y subtopico declaradas arriba
+metadatos <- argendataR::metadata(subtopico = subtopico) %>% 
+  dplyr::filter(grepl(output_name, dataset_archivo)) %>% 
+  distinct(variable_nombre, descripcion) 
+
+
+# Guardo en una variable las columnas del output que queremos escribir
+output_cols <- names(df_output) # lo puedo generar así si tengo df_output
+
+
+etiquetas_nuevas <- data.frame(
+  variable_nombre = c("pais_nombre",
+                      "particip_servicios_pib"),
+  descripcion = c("Nombre de país",
+                  "Participación de los servicios en PIB, en proporción")
+)
+
+descripcion <- armador_descripcion(metadatos = metadatos,
+                                   etiquetas_nuevas = etiquetas_nuevas,
+                                   output_cols = output_cols)
+
+
+
+# Usar write_output con exportar = T para generar la salida
+# Cambiar los parametros de la siguiente funcion segun su caso
+
+df_output %>%
+  argendataR::write_output(
+    output_name = output_name,
+    subtopico = subtopico,
+    fuentes = c(fuente1),
+    analista = analista,
+    pk = c("anio", "iso3"),
+    es_serie_tiempo = T,
+    columna_indice_tiempo = 'anio',
+    nivel_agregacion = "países",
+    descripcion_columnas = descripcion,
+    cambio_nombre_cols = list("pais_nombre" = "iso3_desc_fundar"),
+    unidades = list("particip_servicios_pib" = "unidades"),
+  )
+
