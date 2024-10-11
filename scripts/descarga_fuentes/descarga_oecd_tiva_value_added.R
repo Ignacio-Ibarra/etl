3cacf70ef18054a70f99a11269de977586eb9162
Ignacio-ibarra <ignacioibarra.data.science@gmail.com>
2024-10-11 08:16:54 -0300

    corrijo wrapper oecd y descarga tiva value added

--- scripts/descarga_fuentes/descarga_oecd_tiva_value_added.R
+++ scripts/descarga_fuentes/descarga_oecd_tiva_value_added.R
@@ -3,38 +3,26 @@ rm( list=ls() )  #Borro todos los objetos
 gc()   #Garbage Collection
 
 
+source("scripts/utils/oecd_api.R")
+
 code_name <- str_split_1(rstudioapi::getSourceEditorContext()$path, pattern = "/") %>% tail(., 1)
 
 periodicidad <- months(12)
 fecha_ultima_actualizacion <- as.Date("2024-03-31")
 fecha_actualizar <- fecha_ultima_actualizacion  %m+% periodicidad
 
-require(OECD)
-
-# dataset_code <- "OECD.STI.PIE,DSD_TIVA_MAINLV@DF_MAINLV,1.0/VALU.....A"
-
-# 
-# # Desactivo la verificacion de SSL
-# options(download.file.method="libcurl",
-#         download.file.extra="-k -L --ssl-allow-unsafe-legacy-renegotiation"
-# )
-
-# df_raw <- OECD::get_dataset(dataset_code) # No funciona tira error de R base de la función download.file "HTTP status was 422 Unknown Error"
-
-# df_raw <- read_delim("OECD.STI.PIE_DSD_TIVA_MAINLV@DF_MAINLV_1_0.csv", delim = ";")
-
 
 download_filename <- "OECD.STI.PIE_DSD_TIVA_MAINLV@DF_MAINLV_1_0.csv"
 
 destfile <- glue::glue("{tempdir()}/{download_filename}")
 
-df_raw %>% write_csv(file = destfile)
 
 
+# Define la URL de la API
 url <- "https://sxs-boost-oecd.redpelicans.com/boost-disseminate/v2/sdmx/data/OECD.STI.PIE,DSD_TIVA_MAINLV@DF_MAINLV,1.0/VALU.....A?dimensionAtObservation=AllDimensions"
 
-download.file(url, destfile)
 
+oecd_api.download_data_from_url(url, destfile)
 
 # agregar_fuente_raw(url = url,
 #                    institucion = "OECD",
@@ -46,7 +34,8 @@ download.file(url, destfile)
 #                    api = T
 # )
 
-as.character(fecha_actualizar)
+
+
 actualizar_fuente_raw(id_fuente = 225,
                       fecha_actualizar = as.character(fecha_actualizar),
                       path_raw = download_filename)
