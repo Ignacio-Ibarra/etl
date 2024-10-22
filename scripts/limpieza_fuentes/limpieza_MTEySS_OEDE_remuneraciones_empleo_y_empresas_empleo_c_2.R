aaebc463a48bd3189db6778b3dd99d2e2db7f1e2
Ignacio-ibarra <ignacioibarra.data.science@gmail.com>
2024-10-22 16:21:56 -0300

    limpiezas OEDE remuneraciones empleo y empresas

--- scripts/limpieza_fuentes/limpieza_MTEySS_OEDE_remuneraciones_empleo_y_empresas_empleo_c_2.R
+++ scripts/limpieza_fuentes/limpieza_MTEySS_OEDE_remuneraciones_empleo_y_empresas_empleo_c_2.R
@@ -11,7 +11,7 @@ get_raw_path <- function(codigo){
 }
 
 
-id_fuente <- 237
+id_fuente <- 238
 fuente_raw <- sprintf("R%sC0",id_fuente)
 
 # Función para verificar si el número de NAs en cada fila es mayor o igual a un umbral
@@ -28,7 +28,7 @@ white_cols <- function(df) {
 
 
 
-clean_cuadro_c5 <- function(sheet_name, skip, filas_columnas, names_to, values_to){
+clean_cuadro_c2 <- function(sheet_name, skip, filas_columnas, names_to, values_to){
   
   str_titulos <- readxl::read_excel(get_raw_path(fuente_raw), 
                                     sheet = sheet_name,
@@ -47,8 +47,6 @@ clean_cuadro_c5 <- function(sheet_name, skip, filas_columnas, names_to, values_t
     paste(stats::na.omit(x), collapse = "#")
   })
   
-  cols <- c('ciiu_rev3_4d',cols$concatenado)
-  
   # Leo datos
   sheet_data <- readxl::read_excel(get_raw_path(fuente_raw), 
                                    sheet = sheet_name, 
@@ -57,34 +55,36 @@ clean_cuadro_c5 <- function(sheet_name, skip, filas_columnas, names_to, values_t
   
   sheet_data <- sheet_data[!white_cols(sheet_data)]
   
-  names(sheet_data) <- cols
+  names(sheet_data) <- cols$concatenado
   
   # cuento cantidad de columnas
   num_cols <- length(sheet_data)
   
   # saco las filas que tienen (num_cols - 1) nulos
-  filter_bool <- check_na_threshold(sheet_data, num_cols-1)
-  df <- sheet_data %>% dplyr::filter(!filter_bool) %>% 
-    pivot_longer(!all_of(cols[1:2]),
+  filter_bool <- check_na_threshold(sheet_data, num_cols-4) #Con 4 nulos saco el footer
+  df <- sheet_data %>% dplyr::filter(!filter_bool) %>%
+    pivot_longer(!all_of("Período"),
                  names_to = names_to,
                  values_to = values_to,
-                 names_transform = as.integer,
-                 values_transform = as.numeric) %>% 
-    mutate(cuadro = str_titulos) %>% 
-    janitor::clean_names() 
+                 values_transform = as.numeric) %>%
+    mutate(cuadro = str_titulos) %>%
+    janitor::clean_names() %>%  
+    rename(anio = periodo) %>% 
+    mutate(anio = as.integer(anio),
+           letra = ifelse(letra == "Total", "Z", letra)) 
   
   
   return(df)
 }
 
 
-sheet_name <- "C 5" # Cuadro 4 dice en la celda A1:A1 pero es Cuadro 5
+sheet_name <- "C 2" # Cuadro 4 dice en la celda A1:A1 pero es Cuadro 5
 filas_columnas <- 4:5
 skip <- 6
-names_to <- 'anio'
-values_to <- 'cant_empresas_privadas_activas'
+names_to <- 'letra'
+values_to <- 'cant_promedio_puestos_privados'
 
-df_clean <- clean_cuadro_c5(sheet_name = sheet_name, skip = skip, filas_columnas = filas_columnas, names_to = names_to, values_to = values_to )
+df_clean <- clean_cuadro_c2(sheet_name = sheet_name, skip = skip, filas_columnas = filas_columnas, names_to = names_to, values_to = values_to )
 
 
 
@@ -116,11 +116,10 @@ clean_title <- glue::glue("{titulo.raw} - Cuadro: {sheet_name}")
 #                      nombre = clean_title,
 #                      script = code_name)
 
-actualizar_fuente_clean(id_fuente_clean = 108,
+actualizar_fuente_clean(id_fuente_clean = 110,
                         path_clean = clean_filename,
                         nombre = clean_title, 
-                        script = code_name,
-                        descripcion = "El archivo contiene la cantidad de empresas privadas activas a nivel de 4 dígitos del CIIU Rev. 3, para poder anonimizar hay varios sectores donde no se presentan datos porque la cantidad de empresas es muy pequeña, es por ello que se conservan las fila donde se presenta el valor total de todas las empresas por anio, allí donde rama_de_actividad == 'Total'")
+                        script = code_name)
 
 
 
