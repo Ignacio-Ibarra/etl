8ee3121e0c29fefbdfe2ca0bba8864c167ca9f09
Ignacio-ibarra <ignacioibarra.data.science@gmail.com>
2024-10-28 15:30:14 -0300

    agrego descarga y limpieza de serie poblacion por depto censo 2022, junto con codigos de depto

---
+++ scripts/limpieza_fuentes/limpieza_indec_censo_poblacion_por_depto.R
@@ -0,0 +1,51 @@
+#limpio la memoria
+rm( list=ls() )  #Borro todos los objetos
+gc()   #Garbage Collection
+
+
+get_raw_path <- function(codigo){
+  prefix <- glue::glue("{Sys.getenv('RUTA_FUENTES')}raw/")
+  df_fuentes_raw <- fuentes_raw() 
+  path_raw <- df_fuentes_raw[df_fuentes_raw$codigo == codigo,c("path_raw")]
+  return(paste0(prefix, path_raw))
+}
+
+
+id_fuente <- 243
+fuente_raw <- sprintf("R%sC0",id_fuente)
+
+
+df_clean <- read_csv(get_raw_path(fuente_raw)) %>% 
+  select(depto_id = code_1030, poblacion = value)
+
+
+# Guardado de archivo
+nombre_archivo_raw <- sub("\\.[^.]*$", "", fuentes_raw() %>% 
+                            filter(codigo == fuente_raw) %>% 
+                            select(path_raw) %>% 
+                            pull())
+
+clean_filename <- glue::glue("{nombre_archivo_raw}_CLEAN.parquet")
+
+path_clean <- glue::glue("{tempdir()}/{clean_filename}")
+
+df_clean %>% arrow::write_parquet(., sink = path_clean)
+
+code_name <- str_split_1(rstudioapi::getSourceEditorContext()$path, pattern = "/") %>% tail(., 1)
+
+titulo.raw <- fuentes_raw() %>% 
+  filter(codigo == fuente_raw) %>% 
+  select(nombre) %>% pull()
+
+clean_title <- glue::glue("{titulo.raw}")
+
+# agregar_fuente_clean(id_fuente_raw = id_fuente,
+#                      df = df_clean,
+#                      path_clean = clean_filename,
+#                      nombre = clean_title,
+#                      script = code_name)
+
+actualizar_fuente_clean(id_fuente_clean = 113,
+                        path_clean = clean_filename,
+                        nombre = clean_title, 
+                        script = code_name)
