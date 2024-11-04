b2242e01745192ba9a9b87465c1de453ea2eaa04
Ignacio-ibarra <ignacioibarra.data.science@gmail.com>
2024-11-04 12:25:10 -0300

    modifico script y renombro, agrego descarga de links de balanza de pagos

---
+++ scripts/descarga_fuentes/descarga_indec_balanza_de_pagos_exportaciones_balance.R
@@ -0,0 +1,60 @@
+#limpio la memoria
+rm( list=ls())  #Borro todos los objetos
+gc()   #Garbage Collection
+
+# Función para obtener la ruta del archivo, compatible tanto en RStudio como en la consola
+get_file_location <- function() {
+  # Intenta obtener la ruta del archivo en RStudio
+  if (interactive() && "rstudioapi" %in% rownames(installed.packages())) {
+    return(rstudioapi::getSourceEditorContext()$path)
+  }
+  
+  # Alternativa para obtener la ruta si se usa source()
+  this_file <- (function() { attr(body(sys.function(1)), "srcfile") })()
+  
+  # Si no se obtiene el path (e.g., en consola sin RStudio), asigna un valor por defecto
+  if (!is.null(this_file)) {
+    return(this_file$filename)
+  } else {
+    return("Archivo no especificado o ruta predeterminada")
+  }
+}
+
+code_name <- get_file_location() %>% str_split_1(., pattern = "/") %>% tail(., 1)
+
+periodicidad <- months(3)
+fecha_ultima_actualizacion <- as.Date("2024-09-30")
+fecha_actualizar <- fecha_ultima_actualizacion  %m+% periodicidad
+
+
+source("scripts/utils/indec_scraper_links.R")
+
+
+url <- INDEC.balanza_pagos.extraer_links(id=45, pattern = ".*BOP.*\\.xml")
+
+# Desactivo la verificacion de SSL
+options(download.file.method="libcurl"
+        # , download.file.extra="-k -L --ssl-allow-unsafe-legacy-renegotiation"
+)
+
+download_filename <- "bop.xml"
+
+destfile <- glue::glue("{tempdir()}/{download_filename}")
+
+download.file(url, destfile = destfile, mode = "wb")
+
+# agregar_fuente_raw(url = url,
+#                    institucion = "INDEC",
+#                    nombre = "Balanza de pagos: Exportaciones y balance",
+#                    actualizable = F,
+#                    path_raw = download_filename,
+#                    script = code_name,
+#                    fecha_actualizar = fecha_actualizar,
+#                    api = F
+# )
+
+actualizar_fuente_raw(id_fuente = 245,
+                      url = url, 
+                      fecha_actualizar = as.character(fecha_actualizar),
+                      path_raw = download_filename,
+                      script = code_name)
--- scripts/utils/indec_scraper_links.R
+++ scripts/utils/indec_scraper_links.R
@@ -44,7 +44,7 @@ INDEC.cuentas_nacionales.extraer_links = function(id, pattern){
 # [1] "https://www.indec.gob.ar/ftp/cuadros/economia/serie_cgi_07_24.xls"
 
 
-id <- 119
+
 
 INDEC.poblacion_proyecciones.extraer_links = function(id, pattern, all=FALSE){
   
@@ -79,4 +79,33 @@ INDEC.poblacion_proyecciones.extraer_links = function(id, pattern, all=FALSE){
   
   return(links_info)
   
+}
+
+
+
+INDEC.balanza_pagos.extraer_links = function(id, pattern){
+  
+  url_base <- "https://www.indec.gob.ar"
+  
+  page_url <- paste0(url_base,"/Nivel4/Tema/3/35/",id)
+  
+  # Obtiene el contenido de la página web
+  web_content <- read_html(page_url)
+  
+  # Me quedo con los href
+  links <- web_content %>% 
+    html_nodes("a") %>% 
+    html_attr("href") %>% 
+    str_extract(., pattern = "ftp/.*")
+  
+  # Filtra los links que contienen la URL base
+  url <- paste0(url_base, "/", links[grepl(pattern, links)])
+  
+  # Verificar que 'url' sea un vector de longitud 1 y no vacío
+  if (!is.character(url) || length(url) != 1 || nchar(url) == 0) {
+    stop("Error: 'url' debe ser una cadena de caracteres no vacía y de longitud 1.")
+  }
+  
+  # Si pasa la verificación, devuelve el valor
+  return(url)
 }
