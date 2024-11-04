712f379e678ddb82c77fa20142621ef101598b94
Ignacio-ibarra <ignacioibarra.data.science@gmail.com>
2024-11-04 13:23:04 -0300

    guardo limpieza de BOP

--- scripts/limpieza_fuentes/limpieza_indec_estadisticas_balanza_de_pagos_formato_sdmx.R
+++ scripts/limpieza_fuentes/limpieza_indec_estadisticas_balanza_de_pagos_formato_sdmx.R
@@ -3,6 +3,26 @@ rm( list=ls() )  #Borro todos los objetos
 gc()   #Garbage Collection
 
 
+# Función para obtener la ruta del archivo, compatible tanto en RStudio como en la consola
+get_file_location <- function() {
+  # Intenta obtener la ruta del archivo en RStudio
+  if (interactive() && "rstudioapi" %in% rownames(installed.packages())) {
+    return(rstudioapi::getSourceEditorContext()$path)
+  }
+  
+  # Alternativa para obtener la ruta si se usa source()
+  this_file <- (function() { attr(body(sys.function(1)), "srcfile") })()
+  
+  # Si no se obtiene el path (e.g., en consola sin RStudio), asigna un valor por defecto
+  if (!is.null(this_file)) {
+    return(this_file$filename)
+  } else {
+    return("Archivo no especificado o ruta predeterminada")
+  }
+}
+
+code_name <- get_file_location() %>% str_split_1(., "/") %>% tail(.,1)
+
 get_raw_path <- function(codigo){
   prefix <- glue::glue("{Sys.getenv('RUTA_FUENTES')}raw/")
   df_fuentes_raw <- fuentes_raw() 
@@ -32,22 +52,21 @@ path_clean <- glue::glue("{tempdir()}/{clean_filename}")
 
 df_clean %>% arrow::write_parquet(., sink = path_clean)
 
-code_name <- str_split_1(rstudioapi::getSourceEditorContext()$path, pattern = "/") %>% tail(., 1)
 
 titulo.raw <- fuentes_raw() %>% 
   filter(codigo == fuente_raw) %>% 
   select(nombre) %>% pull()
 
-clean_title <- glue::glue("{titulo.raw}")
+clean_title <- glue::glue("{titulo.raw} - Dataset limpio")
 
 # agregar_fuente_clean(id_fuente_raw = id_fuente,
 #                      df = df_clean,
-#                      path_clean = clean_filename,
 #                      nombre = clean_title,
+#                      path_clean = clean_filename,
 #                      script = code_name,
 #                      )
 
-id_fuente_clean <- 115
+id_fuente_clean <- 133
 codigo_fuente_clean <- sprintf("R%sC%s", id_fuente, id_fuente_clean)
 
 
