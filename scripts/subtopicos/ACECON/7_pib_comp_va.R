e4d9d1d322a43f97dc672124531a49cd6b0548de
Ignacio-ibarra <ignacioibarra.data.science@gmail.com>
2024-11-05 14:06:18 -0300

    Agrego comparación, mejoro descripción de variables, corrijo unidad de indicador, borro nulos en columna 'valor' y corrijo parámetros de write_output

--- scripts/subtopicos/ACECON/7_pib_comp_va.R
+++ scripts/subtopicos/ACECON/7_pib_comp_va.R
@@ -1,18 +1,25 @@
 # 7_pib_comp_va
 # descripcion
 
-# vars config del script
-output_name <- "7_pib_comp_va.csv"
-subtopico <- "ACECON"
-# periodo, etc.,
+#limpio la memoria
+rm( list=ls() )  #Borro todos los objetos
+gc()   #Garbage Collection
 
-# Insumos -------
+
+subtopico <- "ACECON"
+output_name <- "7_pib_comp_va"
+analista = "Daniel Schteingart y Juan Gabriel Juara"
+fuente1 <- "R36C13"
+fuente2 <- "R38C7"
+
+
 
 # sectores a precios basicos como % del pib a precio de mercado y PIB a precio de basicos como % respecto a precio de mercado
-pbisectores_fnys <- read_csv(argendataR::get_clean_path("R36C13"))
+pbisectores_fnys <- arrow::read_parquet(argendataR::get_clean_path(fuente1))
 
 # vab por sectores a precios corrientes en millones de pesos corrientes 
-pbisectores_indec <- read_csv(argendataR::get_clean_path("R38C7"))
+pbisectores_indec <- arrow::read_parquet(argendataR::get_clean_path(fuente2))
+
 
 # Procesamiento -------
 
@@ -143,8 +150,6 @@ pbisectores_indec <- pbisectores_indec %>%
 colnames(pbisectores_indec) <- gsub("prop_._|prop_", "",
      colnames(pbisectores_indec))
 
-pbisectores_indec <- pbisectores_indec %>% 
-  select(-producto_interno_bruto)
 
 pbisectores_fnys <- pbisectores_fnys %>% 
   rename( otros_servicios = administrac_publica_y_defensa_y_servicios_sociales_comunales_y_personales_otros_servicios,
@@ -162,48 +167,121 @@ df_output <- bind_rows(pbisectores_fnys,
                         pbisectores_indec) 
 
 
-df_output %>% 
-  filter(anio == 2004) %>% 
-  pivot_longer(cols = -anio) %>% 
-  group_by(name) %>% 
-  summarise(sd = stats::sd(value)/mean(value))
-
 df_output <- df_output %>% 
   pivot_longer(cols = -anio, names_to = "sector", values_to = "valor")
 
 df_output <- df_output %>% 
   mutate(valor = round(valor, 4)) %>% 
-  distinct()
+  distinct() %>% drop_na(valor)
 
 
 # el control por el momento no tiene version previa de comparacion
 
-# df_anterior <- argendataR::descargar_output(nombre = output_name,
-#                                             subtopico = subtopico,
-#                                             entrega_subtopico = "primera_entrega")
+df_anterior <- argendataR::descargar_output(nombre = output_name,
+                                            subtopico = subtopico,
+                                            entrega_subtopico = "primera_entrega")
 # 
 # #-- Controlar Output ----
 # 
-# comparacion <- argendataR::comparar_outputs(
-#   df_output,
-#   df_anterior,
-#   pk = c("anio", "sector"),
-#   drop_joined_df = F
-# )
+comparacion <- argendataR::comparar_outputs(
+  df_output,
+  df_anterior,
+  pk = c("anio", "sector"),
+  drop_joined_df = F
+)
 
 
 
-# Write output ------
+#-- Exportar Output ----
+
+armador_descripcion <- function(metadatos, etiquetas_nuevas = data.frame(), output_cols){
+  # metadatos: data.frame sus columnas son variable_nombre y descripcion y 
+  # proviene de la info declarada por el analista 
+  # etiquetas_nuevas: data.frame, tiene que ser una dataframe con la columna 
+  # variable_nombre y la descripcion
+  # output_cols: vector, tiene las columnas del dataset que se quiere escribir
+  
+  etiquetas <- metadatos %>% 
+    dplyr::filter(variable_nombre %in% output_cols) 
+  
+  
+  etiquetas <- etiquetas %>% 
+    bind_rows(etiquetas_nuevas)
+  
+  
+  diff <- setdiff(output_cols, etiquetas$variable_nombre)
+  
+  stopifnot(`Error: algunas columnas de tu output no fueron descriptas` = length(diff) == 0)
+  
+  # En caso de que haya alguna variable que le haya cambiado la descripcion pero que
+  # ya existia se va a quedar con la descripcion nueva. 
+  
+  etiquetas <- etiquetas %>% 
+    group_by(variable_nombre) %>% 
+    filter(if(n() == 1) row_number() == 1 else row_number() == n()) %>%
+    ungroup()
+  
+  etiquetas <- stats::setNames(as.list(etiquetas$descripcion), etiquetas$variable_nombre)
+  
+  return(etiquetas)
+  
+}
+
+# Tomo las variables output_name y subtopico declaradas arriba
+metadatos <- argendataR::metadata(subtopico = subtopico) %>% 
+  dplyr::filter(grepl(paste0(output_name,".csv"), dataset_archivo)) %>% 
+  distinct(variable_nombre, descripcion) 
 
 
-df_output %>% 
-  write_output(output_name = output_name,
-               fuentes = c("R36C13", "R38C7"),
-               subtopico = "ACECON",
-               analista = "",
-               pk = c("anio", "sector"),
-               es_serie_tiempo = T,
-               columna_indice_tiempo = "anio",
-               nivel_agregacion = "pais",
-               etiquetas_indicadores =  list("sector" = "Sector"),
-               unidades = list("valor"="Porcentaje del PIB a precios básicos") )
+# Guardo en una variable las columnas del output que queremos escribir
+output_cols <- names(df_output) # lo puedo generar así si tengo df_output
+
+
+etiquetas_nuevas <- data.frame(
+  variable_nombre = c("anio","sector", "valor"),
+  descripcion = c("Año de referencia",
+                  "Sector de la actividad económica",
+                  "Participación en el VAB a precios básicos, en pesos corrientes")
+)
+
+descripcion <- armador_descripcion(metadatos = metadatos,
+                                   etiquetas_nuevas = etiquetas_nuevas,
+                                   output_cols = output_cols)
+
+
+colectar_fuentes <- function(pattern = "^fuente.*"){
+  
+  # Genero un vector de codigos posibles
+  posibles_codigos <- c(fuentes_raw()$codigo,fuentes_clean()$codigo)
+  
+  # Usar ls() para buscar variables en el entorno global
+  variable_names <- ls(pattern = pattern, envir = globalenv())
+  
+  # Obtener los valores de esas variables
+  valores <- unlist(mget(variable_names, envir = globalenv()))
+  
+  # Filtrar aquellas variables que sean de tipo character (string)
+  # Esto es para que la comparacion sea posible en la linea de abajo
+  strings <- valores[sapply(valores, is.character)]
+  
+  # solo devuelvo las fuentes que existen
+  return(valores[valores %in% posibles_codigos])
+}
+# Usar write_output con exportar = T para generar la salida
+# Cambiar los parametros de la siguiente funcion segun su caso
+
+df_output %>%
+  argendataR::write_output(
+    output_name = output_name,
+    subtopico = subtopico,
+    fuentes = colectar_fuentes(),
+    analista = analista,
+    pk = c("anio", "sector"),
+    es_serie_tiempo = T,
+    control = comparacion, 
+    columna_indice_tiempo = 'anio',
+    descripcion_columnas = descripcion,
+    unidades = list("valor"="porcentaje")
+  )
+
+
