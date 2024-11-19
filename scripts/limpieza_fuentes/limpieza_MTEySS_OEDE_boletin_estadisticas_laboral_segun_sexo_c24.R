b3d86aeedf060d30ca7d3e377d2d4c2c74f5626e
Ignacio-ibarra <ignacioibarra.data.science@gmail.com>
2024-11-19 20:16:53 -0300

    Limpieza cuadro C 2.4 de Boletin de Estadísticas Laborales según sexo de OEDE

---
+++ scripts/limpieza_fuentes/limpieza_MTEySS_OEDE_boletin_estadisticas_laboral_segun_sexo_c24.R
@@ -0,0 +1,163 @@
+#limpio la memoria
+rm( list=ls() )  #Borro todos los objetos
+gc()   #Garbage Collection
+
+
+
+#limpio la memoria
+rm( list=ls())  #Borro todos los objetos
+gc()   #Garbage Collection
+
+# Función para obtener la ruta del archivo, compatible tanto en RStudio como en la consola
+get_file_location <- function() {
+  # Intenta obtener la ruta del archivo en RStudio
+  if (interactive() && "rstudioapi" %in% rownames(installed.packages())) {
+    return(rstudioapi::getSourceEditorContext()$path)
+  }
+  
+  # Alternativa para obtener la ruta si se usa source()
+  this_file <- (function() { attr(body(sys.function(1)), "srcfile") })()
+  
+  # Si no se obtiene el path (e.g., en consola sin RStudio), asigna un valor por defecto
+  if (!is.null(this_file)) {
+    return(this_file$filename)
+  } else {
+    return("Archivo no especificado o ruta predeterminada")
+  }
+}
+
+code_name <- get_file_location() %>% str_split_1(., pattern = "/") %>% tail(., 1)
+
+
+id_fuente <- 235
+fuente_raw <- sprintf("R%sC0",id_fuente)
+
+# Función para verificar si el número de NAs en cada fila es mayor o igual a un umbral
+check_na_threshold <- function(df, threshold) {
+  apply(df, 1, function(row) {
+    sum(is.na(row)) >= threshold
+  })
+}
+
+white_cols <- function(df) {
+  sapply(df, function (col) all(is.na(col)))
+}
+
+
+clean_cuadros_c24 <- function(sheet_name, skip, filas_columnas, names_to, values_to){
+  
+  str_titulos <- readxl::read_excel(get_raw_path(fuente_raw), 
+                                    sheet = sheet_name,
+                                    range = "A1:A3",
+                                    col_names = F)
+  
+  seccion_str <- str_titulos$...1[1]
+  cuadro_str <- str_titulos$...1[2]
+  detalle_str <- str_titulos$...1[3]
+  
+  
+  
+  cols_ <- readxl::read_excel(argendataR::get_raw_path(fuente_raw), 
+                              sheet = sheet_name,
+                              col_names = F) %>% slice(filas_columnas)
+  
+  cols <- cols_[!white_cols(cols_)] %>%
+    t() %>% # Transponer
+    as.data.frame() %>% 
+    fill(V1, .direction = "down")
+  
+  cols$concatenado <- apply(cols, 1, function(x) {
+    paste(stats::na.omit(x), collapse = "#")
+  })
+  
+  
+  
+  # Leo datos
+  sheet_data <- readxl::read_excel(get_raw_path(fuente_raw), 
+                                   sheet = sheet_name, 
+                                   skip = skip, 
+                                   col_names = F)
+  
+  sheet_data <- sheet_data[!white_cols(sheet_data)]
+  
+  cols <- c("trimestre_anio", cols$concatenado[-1])
+  
+  names(sheet_data) <- cols
+  
+  # cuento cantidad de columnas
+  num_cols <- length(sheet_data)
+  
+  # saco las filas que tienen (num_cols - 1) nulos
+  filter_bool <- check_na_threshold(sheet_data, num_cols-1)
+  data <- sheet_data %>% dplyr::filter(!filter_bool)
+  
+  # pivoteo datos y genero columna con nombre de provincia
+  df <- data %>% pivot_longer(cols = -all_of("trimestre_anio"), 
+                              names_to = names_to,
+                              names_sep = "#",
+                              values_to = values_to, 
+                              values_transform = as.numeric) %>% 
+    mutate(seccion = seccion_str, 
+           cuadro = cuadro_str,
+           detalle = detalle_str) %>% 
+    janitor::clean_names()
+  
+  
+  return(df)
+}
+
+
+sheet_name <- "C 2.4"
+skip <- 6
+filas_columnas <- 5:6
+names_to <- c('rama_de_actividad','sexo')
+values_to <- 'cantidad_puestos_privados_registrados'
+
+df_clean <- clean_cuadros_c24(sheet_name = sheet_name, skip = skip, filas_columnas = filas_columnas, names_to = names_to, values_to = values_to )
+
+
+
+# Guardado de archivo
+nombre_archivo_raw <- sub("\\.[^.]*$", "", fuentes_raw() %>% 
+                            filter(codigo == fuente_raw) %>% 
+                            select(path_raw) %>% 
+                            pull())
+
+normalized_sheet_name <- sheet_name %>% janitor::make_clean_names(.)
+
+clean_filename <- glue::glue("{nombre_archivo_raw}_{normalized_sheet_name}_CLEAN.parquet")
+
+path_clean <- glue::glue("{tempdir()}/{clean_filename}")
+
+df_clean %>% arrow::write_parquet(., sink = path_clean)
+
+code_name <- str_split_1(rstudioapi::getSourceEditorContext()$path, pattern = "/") %>% tail(., 1)
+
+titulo.raw <- fuentes_raw() %>% 
+  filter(codigo == fuente_raw) %>% 
+  select(nombre) %>% pull()
+
+clean_title <- glue::glue("{titulo.raw} - Cuadro: {sheet_name}")
+
+# agregar_fuente_clean(id_fuente_raw = id_fuente,
+#                      df = df_clean,
+#                      path_clean = clean_filename,
+#                      nombre = clean_title,
+#                      script = code_name)
+
+id_fuente_clean <- 147
+codigo_fuente_clean <- sprintf("R%sC%s", id_fuente, id_fuente_clean)
+
+
+df_clean_anterior <- arrow::read_parquet(get_clean_path(codigo = codigo_fuente_clean ))
+
+comparacion <- comparar_fuente_clean(df_clean,
+                                     df_clean_anterior,
+                                     pk = c("trimestre_anio","rama_de_actividad","sexo")
+)
+
+actualizar_fuente_clean(id_fuente_clean = id_fuente_clean,
+                        path_clean = clean_filename,
+                        nombre = clean_title, 
+                        script = code_name,
+                        comparacion = comparacion)
