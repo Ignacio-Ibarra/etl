60fb33e9e6010326e6c7e622d7518e64acf3a395
Ignacio-ibarra <ignacioibarra.data.science@gmail.com>
2025-01-14 13:05:27 -0300

    corrijo script y modifico fecha actualizacion

--- scripts/descarga_fuentes/descarga_indec_agregados_macroeconomicos_series_vab_vbp_por_sector.R
+++ scripts/descarga_fuentes/descarga_indec_agregados_macroeconomicos_series_vab_vbp_por_sector.R
@@ -1,8 +1,12 @@
+#limpio la memoria
+rm( list=ls() )  #Borro todos los objetos
+gc()   #Garbage Collection
+
 code_path <- this.path::this.path()
 code_name <- code_path %>% str_split_1(., pattern = "/") %>% tail(., 1)
 
 periodicidad <- months(3)
-fecha_ultima_actualizacion <- as.Date("2024-09-18")
+fecha_ultima_actualizacion <- as.Date("2024-12-18")
 fecha_actualizar <- fecha_ultima_actualizacion  %m+% periodicidad
 
 source("scripts/utils/indec_scraper_links.R")
@@ -11,13 +15,17 @@ agregados_macro_id <- 47
 
 pattern_vbp_vab <- ".*sh_VBP_VAB_.*\\.xls"
 
-url <- INDEC.cuentas_nacionales.extraer_links(id = agregados_macro_id, pattern = pattern_vbp_vab)
+result <- INDEC.cuentas_nacionales.extraer_links(id = agregados_macro_id, pattern = pattern_vbp_vab)
+
+url <- result$url
 
 download_filename <- url %>% str_extract(., "sh.*\\.xls.*") # esta linea no debería ser así, debería proveer una string estatica
 
 destfile <- glue::glue("{tempdir()}/{download_filename}")
 
 download.file(url, destfile = destfile, mode = "wb")
+
+nombre = glue::glue("Cuentas Nacionales. Agregados Macroeconómicos (PIB). {result$text}")
 
 # agregar_fuente_raw(url = url,
 #                    institucion = "INDEC",
@@ -30,6 +38,8 @@ download.file(url, destfile = destfile, mode = "wb")
 # )
 
 actualizar_fuente_raw(id_fuente = 223,
+                      url = url,
+                      nombre = nombre,
                       fecha_actualizar = fecha_actualizar,
                       path_raw = download_filename,
                       script = code_name) 
