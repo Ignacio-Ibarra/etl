673c1c6923f5e87aecb7de0531f2e26c4110c29b
Ignacio-ibarra <ignacioibarra.data.science@gmail.com>
2025-03-05 10:16:17 -0300

    cambio fuente: sale R240C0, entra R107C0

--- scripts/subtopicos/ESTPRO/22_densidad_empresarial_depto.R
+++ scripts/subtopicos/ESTPRO/22_densidad_empresarial_depto.R
@@ -12,35 +12,23 @@ output_name <- "densidad_empresarial_depto"
 analista = "Gisella Pascuariello"
 
 fuente1 <- "R243C113"
-fuente2 <- "R240C0"
+fuente2 <- "R107C0"
+fuente3 <- "R248C0"
+fuente4 <- "R244C0"
+
+df_pob_censo <- arrow::read_parquet(argendataR::get_clean_path(fuente1)) %>% 
+  mutate(depto_id = str_pad(depto_id, width = 5, side = 'left', pad ="0")) 
 
 
-get_clean_path <- function(codigo){
-  prefix <- glue::glue("{Sys.getenv('RUTA_FUENTES')}clean/")
-  df_fuentes_clean <- fuentes_clean() 
-  path_clean <- df_fuentes_clean[df_fuentes_clean$codigo == codigo,c("path_clean")]
-  return(paste0(prefix, path_clean))
-}
-
-
-get_raw_path <- function(codigo){
-  prefix <- glue::glue("{Sys.getenv('RUTA_FUENTES')}raw/")
-  df_fuentes_raw <- fuentes_raw() 
-  path_raw <- df_fuentes_raw[df_fuentes_raw$codigo == codigo,c("path_raw")]
-  return(paste0(prefix, path_raw))
-}
-
-df_pob_censo <- arrow::read_parquet(get_clean_path(fuente1))
-
-
-
-df_establecimientos <- read_csv(get_raw_path(fuente2)) %>% 
-  mutate(depto_id = str_pad(in_departamentos, width = 5, side = 'left', pad ="0")) %>% 
+df_establecimientos <- read_csv(argendataR::get_raw_path(fuente2)) %>% 
+  mutate(depto_id = str_pad(in_departamentos, width = 5, side = 'left', pad ="0"),
+         provincia_id = str_pad(provincia_id, width = 2, side = "left", pad = "0")) %>% 
   dplyr::filter(anio == 2022) %>% 
-  group_by(depto_id, depto_nombre = departamento, provincia_id, provincia) %>% 
+  group_by(anio, depto_id, provincia_id) %>% 
   summarise(
-    establecimientos = sum(Establecimientos, na.rm = T),
+    establecimientos = n(),
     ) %>% 
+  ungroup() %>% 
   mutate(
     depto_id = case_when(
       depto_id == "94007" ~ "94008", # Cambio codigo de Rio Grande en Censo 2022, CEP lo tiene mal
@@ -49,24 +37,36 @@ df_establecimientos <- read_csv(get_raw_path(fuente2)) %>%
       TRUE ~ depto_id
     )
   )
-    
+
+
+diccionario_depto <- argendataR::get_raw_path(fuente3) %>% 
+  read.csv(., sep = ";") %>% 
+  mutate(depto_id = str_pad(IN1, width = 5, side = 'left', pad ="0")) %>% 
+  select(depto_id, depto_nombre = NAM) %>% 
+  filter(!((depto_id == "50035") & (depto_nombre == "San Martín")))
+
+
+
+provincia_desc <- argendataR::get_raw_path(fuente4) %>% 
+  read.csv(., sep = ",") %>% 
+  distinct(provincia_id = cod_pcia, provincia = nom_pcia) %>% 
+  mutate(provincia_id = str_pad(provincia_id, width = 2, side = "left", pad = "0"))  
 
 df_output <- df_pob_censo %>%
+  left_join(diccionario_depto, join_by(depto_id)) %>%
   left_join(df_establecimientos, join_by(depto_id)) %>% 
-  filter(!is.na(establecimientos)) %>% 
-  mutate(densidad_emp = 1000*establecimientos / poblacion,
-         anio = 2022) %>% 
-  select(anio, id_depto = depto_id, departamento = depto_nombre, provincia_id, provincia, densidad_emp) %>% 
-  mutate(provincia_id = str_pad(provincia_id, width = 2, side = 'left', pad ="0"))
+  mutate(densidad_emp = 1000*establecimientos / poblacion) %>% 
+  left_join(provincia_desc, join_by(provincia_id)) %>% 
+  select(anio, id_depto = depto_id, departamento = depto_nombre, provincia_id, provincia, densidad_emp) 
   
 
 
-df_anterior <- argendataR::descargar_output(nombre =output_name, subtopico = subtopico, entrega_subtopico = "primera_entrega") 
+df_anterior <- argendataR::descargar_output(nombre =output_name, subtopico = subtopico) 
 
 comparacion <- argendataR::comparar_outputs(
   df_anterior = df_anterior,
   df = df_output,
-  pk = c("id_depto"), # variables pk del dataset para hacer el join entre bases
+  pk = c("anio","id_depto"), # variables pk del dataset para hacer el join entre bases
   drop_joined_df =  F
 )
 
@@ -157,33 +157,12 @@ df_output %>%
     descripcion_columnas = descripcion,
     unidades = list("densidad_emp" = "unidades"),
     aclaraciones = "Se modificó el dato población, antes era proyeccion 2010-2025 y ahora se usó el dato del Censo 2022,
-    la columna 'anio' se mantiene a los fines de identificar cual es el anio de las observaciones"
+    la columna 'anio' se mantiene a los fines de identificar cual es el anio de las observaciones. Se modificó la fuente, 
+    antes se usaba la R240C0 ahora se usa R107C0 para establecimientos por depto, eso genera diferencias. 
+    Pero la fuente anterior estaba mal"
   )
 
 
-# joined_df <- comparacion$joined_df
-# 
-# # Conozco cuales son mis primary keys
-# primary_keys <- c('id_depto')
-# 
-# # Genero una columna de etiquetas para el tooltip, con las columnas que son pk
-# joined_df$label <- apply(joined_df[primary_keys], 1, function(row) paste(row, collapse = " - "))
-# 
-# # Conozco la columna numérica que quiero comparar.
-# col_comparar <- 'densidad_emp'
-# cols_comp <- paste0(col_comparar, c('.x','.y'))
-# col_x <- cols_comp[1]
-# col_y <- cols_comp[2]
-# 
-# # Genero scatter de ggplot
-# scatter <-  ggplot(joined_df, aes(x = !!sym(col_x), y = !!sym(col_y), label = label))+
-#   geom_point() +
-#   geom_abline(slope = 1,color = "red", alpha = 0.7) +
-#   theme_minimal() +
-#   labs(title = "Scatterplot dinámico", x = cols_comp[1], y = cols_comp[2])
-# 
-# # Agregar interactividad con plotly
-# interactive_scatter <- plotly::ggplotly(scatter, tooltip = 'label')
-# 
-# # Mostrar el scatter interactivo
-# interactive_scatter
+argendataR::mandar_data("densidad_empresarial_depto.csv", subtopico = subtopico, branch ="dev")
+argendataR::mandar_data("densidad_empresarial_depto.json", subtopico = subtopico, branch ="dev")
+
