052e66bcaa4f736a53784c5a964b55d4b0401750
Ignacio-ibarra <ignacioibarra.data.science@gmail.com>
2025-03-31 09:46:11 -0300

    descarga y limpieza Cifras CONICET id 912

---
+++ scripts/limpieza_fuentes/limpieza_conicet_cifras_912.R
@@ -0,0 +1,61 @@
+#limpio la memoria
+rm( list=ls() )  #Borro todos los objetos
+gc()   #Garbage Collection
+
+code_path <- this.path::this.path()
+code_name <- code_path %>% str_split_1(., pattern = "/") %>% tail(., 1)
+
+
+id_fuente <- 354
+fuente_raw <- sprintf("R%sC0",id_fuente)
+
+# Guardado de archivo
+nombre_archivo_raw <- str_split_1(fuentes_raw() %>% 
+                                    filter(codigo == fuente_raw) %>% 
+                                    select(path_raw) %>% 
+                                    pull(), pattern = "\\.")[1]
+
+titulo.raw <- fuentes_raw() %>% 
+  filter(codigo == fuente_raw) %>% 
+  select(nombre) %>% pull()
+
+
+df_clean <- argendataR::get_raw_path(fuente_raw) %>% 
+  readxl::read_excel(.) %>% 
+  janitor::clean_names() %>% 
+  rename(id_persona = id_dataset)
+
+
+clean_filename <- glue::glue("{nombre_archivo_raw}_CLEAN.parquet")
+
+clean_title <- glue::glue("{titulo.raw}")
+
+path_clean <- glue::glue("{tempdir()}/{clean_filename}")
+
+df_clean %>% arrow::write_parquet(., sink = path_clean)
+
+
+# agregar_fuente_clean(id_fuente_raw = id_fuente,
+#                      path_clean = clean_filename,
+#                      nombre = clean_title,
+#                      script = code_name)
+
+
+
+id_fuente_clean <- 230
+codigo_fuente_clean <- sprintf("R%sC%s", id_fuente, id_fuente_clean)
+
+
+df_clean_anterior <- arrow::read_parquet(get_clean_path(codigo = codigo_fuente_clean ))
+
+
+comparacion <- comparar_fuente_clean(df_clean,
+                                     df_clean_anterior,
+                                     pk = c('id_persona')
+)
+
+actualizar_fuente_clean(id_fuente_clean = id_fuente_clean,
+                        path_clean = clean_filename,
+                        nombre = clean_title, 
+                        script = code_name,
+                        comparacion = comparacion)
