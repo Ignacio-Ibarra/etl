cbd699cf972aed60caeefde3fc66552c1f9cd8a4
Ignacio-ibarra <ignacioibarra.data.science@gmail.com>
2025-04-03 15:36:37 -0300

    cambio nombre script y agrego scraper en descarga

---
+++ scripts/descarga_fuentes/descarga_indec_proy_pob_nacional.R
@@ -0,0 +1,46 @@
+# limpio la memoria
+rm( list=ls() )  #Borro todos los objetos
+gc()   #Garbage Collection
+
+code_path <- this.path::this.path()
+code_name <- code_path %>% str_split_1(., pattern = "/") %>% tail(., 1)
+
+periodicidad <- months(3)
+fecha_ultima_actualizacion <- as.Date("2024-12-20")
+fecha_actualizar <- "Sin informacion"
+
+
+source("scripts/utils/indec_scraper_links.R")
+
+
+result <- INDEC.poblacion_proyecciones_nacionales.extraer_links(id = 84, pattern = "c1_proyecciones_nac_.*\\.xls$")
+
+url <- result$url
+
+nombre <- result$text
+
+institucion <- "Instituto Nacional de Estadísticas y Censo"
+
+download_filename <- basename(url)
+
+destfile <- glue::glue("{tempdir()}/{download_filename}")
+
+download.file(url, destfile = destfile, mode = "wb")
+
+# agregar_fuente_raw(url = url,
+#                    institucion = institucion,
+#                    nombre = nombre,
+#                    actualizable = T,
+#                    path_raw = download_filename,
+#                    script = code_name,
+#                    fecha_actualizar = fecha_actualizar,
+#                    api = F
+# )
+
+actualizar_fuente_raw(id_fuente = 39,
+                      url = url, 
+                      nombre = nombre, 
+                      institucion = institucion,
+                      fecha_actualizar = fecha_actualizar,
+                      path_raw = download_filename,
+                      script = code_name)
