78cbc6adba4636e903dc5679f916828d4b4ee136
Ignacio-ibarra <ignacioibarra.data.science@gmail.com>
2025-04-22 11:04:17 -0300

    Descarga y limpieza de ENGHO 2017/2018. Tablas articulos, gastos, hogares, personas.

---
+++ scripts/descarga_fuentes/descarga_indec_engho_2018_articulos.R
@@ -0,0 +1,47 @@
+# limpio la memoria
+rm( list=ls() )  #Borro todos los objetos
+gc()   #Garbage Collection
+
+code_path <- this.path::this.path()
+code_name <- code_path %>% str_split_1(., pattern = "/") %>% tail(., 1)
+
+periodicidad <- months(24)
+fecha_ultima_actualizacion <- as.Date("2020-12-11")
+fecha_actualizar <- "Sin informacion" 
+
+
+source("scripts/utils/indec_scraper_links.R")
+
+resultado <- INDEC.bases_microdatos(id = 4) %>% 
+  dplyr::filter(grepl("engho2018_articulos.zip", url))
+
+url <- resultado$url
+
+nombre <- glue::glue("{resultado$encuesta}. {resultado$titulo}")
+
+institucion <- "Instituto Nacional de Estadística y Censos"
+
+download_filename <- basename(url)
+
+destfile <- glue::glue("{tempdir()}/{download_filename}")
+
+download.file(url, destfile)
+
+ 
+# agregar_fuente_raw(url = url,
+#                    institucion = institucion,
+#                    nombre = nombre,
+#                    actualizable = F,
+#                    path_raw = download_filename,
+#                    script = code_name,
+#                    fecha_actualizar = fecha_actualizar,
+#                    api = F
+# )
+
+actualizar_fuente_raw(id_fuente = 393,
+                      url = url, 
+                      nombre = nombre, 
+                      institucion = institucion,
+                      fecha_actualizar = fecha_actualizar,
+                      path_raw = download_filename,
+                      script = code_name)
