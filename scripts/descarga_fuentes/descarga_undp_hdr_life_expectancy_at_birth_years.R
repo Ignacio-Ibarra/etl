7725127f94198890e1cda9003f3fd524a2d9fbc0
Ignacio-ibarra <ignacioibarra.data.science@gmail.com>
2025-06-04 17:00:39 -0300

    corrección codigo hdr life expectancy at birth

--- scripts/descarga_fuentes/descarga_undp_hdr_life_expectancy_at_birth_years.R
+++ scripts/descarga_fuentes/descarga_undp_hdr_life_expectancy_at_birth_years.R
@@ -1,33 +1,15 @@
-
 #limpio la memoria
-rm( list=ls())  #Borro todos los objetos
+rm( list=ls() )  #Borro todos los objetos
 gc()   #Garbage Collection
 
-# Función para obtener la ruta del archivo, compatible tanto en RStudio como en la consola
-get_file_location <- function() {
-  # Intenta obtener la ruta del archivo en RStudio
-  if (interactive() && "rstudioapi" %in% rownames(installed.packages())) {
-    return(rstudioapi::getSourceEditorContext()$path)
-  }
-  
-  # Alternativa para obtener la ruta si se usa source()
-  this_file <- (function() { attr(body(sys.function(1)), "srcfile") })()
-  
-  # Si no se obtiene el path (e.g., en consola sin RStudio), asigna un valor por defecto
-  if (!is.null(this_file)) {
-    return(this_file$filename)
-  } else {
-    return("Archivo no especificado o ruta predeterminada")
-  }
-}
-
-code_name <- get_file_location() %>% str_split_1(., pattern = "/") %>% tail(., 1)
-
-source("./scripts/utils/human_development_report_undp_api.R")
-
+code_path <- this.path::this.path()
+code_name <- code_path %>% str_split_1(., pattern = "/") %>% tail(., 1)
 
 periodicidad <- months(12)
-fecha_actualizar <- as.Date("2025-04-30")
+fecha_ultima_actualizacion <- as.Date("2025-04-30")
+fecha_actualizar <- fecha_ultima_actualizacion  %m+% periodicidad
+
+source("./scripts/utils/human_development_report_undp_api.R")
 
 
 countries <- hdr_api.get_metadata(apikey = Sys.getenv("hdr_key"),
@@ -76,5 +58,6 @@ nombre <- glue::glue("{hdr_indicator$name}. Human Development Report (Revision {
 
 actualizar_fuente_raw(id_fuente = 283,
                       fecha_actualizar = as.character(fecha_actualizar),
-                      path_raw = download_filename
+                      path_raw = download_filename,
+                      script = code_name
 )
