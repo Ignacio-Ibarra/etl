89aa9aafa273943864d8109fa907886eda8b8f1c
Ignacio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-09-23 19:01:28 -0300

    scripts output

--- scripts/subtopicos/DEMOGR/12_expectativa_vida_al_nacer_paises.R
+++ scripts/subtopicos/DEMOGR/12_expectativa_vida_al_nacer_paises.R
@@ -16,19 +16,93 @@ geo_front <- argendataR::get_nomenclador_geografico_front() %>%
   select(geocodigoFundar = geocodigo, geonombreFundar = name_long)
 
 df_output <- df_wpp %>% 
-  dplyr::filter(iso3_code != "",
+  dplyr::filter((trimws(iso3_code) != '') | (loc_id == 904),
                 !is.na(iso3_code),
-                time <= year(Sys.Date())) %>% 
-  left_join(geo_front, join_by(iso3_code == geocodigoFundar)) %>% 
-  select(anio = time, geocodigoFundar = iso3_code, geonombreFundar, exp_vida_al_nacer = l_ex) 
+                time <= year(Sys.Date()))  %>% 
+  mutate(geocodigoFundar = ifelse(iso3_code == '', 'LCN', iso3_code)) %>% 
+  left_join(geo_front, join_by(geocodigoFundar)) %>% 
+  select(anio = time, geocodigoFundar, geonombreFundar, exp_vida_al_nacer = l_ex) 
 
 
 
-df_plot <- df_output %>% 
-  dplyr::filter(geonombreFundar %in% c("Argentina", "Brasil", "Chile", "Perú", "España", "Uruguay"))
+df_anterior <- argendataR::descargar_output(nombre = output_name,
+                                            subtopico = subtopico)
 
-ggplot(df_plot, aes(x=anio, y = exp_vida_al_nacer, color = geonombreFundar)) + 
-  geom_line() + 
-  theme_minimal()+
-  ylim(0, NA)
+pks <- c('anio', 'geocodigoFundar')
+
+comparacion <- argendataR::comparar_outputs(
+  df = df_output,
+  df_anterior = df_anterior,
+  nombre = output_name,
+  pk = pks
+)
+
+
+armador_descripcion <- function(metadatos, etiquetas_nuevas = data.frame(), output_cols){
+  # metadatos: data.frame sus columnas son variable_nombre y descripcion y 
+  # proviene de la info declarada por el analista 
+  # etiquetas_nuevas: data.frame, tiene que ser una dataframe con la columna 
+  # variable_nombre y la descripcion
+  # output_cols: vector, tiene las columnas del dataset que se quiere escribir
+  
+  etiquetas <- metadatos %>% 
+    dplyr::filter(variable_nombre %in% output_cols) 
+  
+  
+  etiquetas <- etiquetas %>% 
+    bind_rows(etiquetas_nuevas)
+  
+  
+  diff <- setdiff(output_cols, etiquetas$variable_nombre)
+  
+  stopifnot(`Error: algunas columnas de tu output no fueron descriptas` = length(diff) == 0)
+  
+  # En caso de que haya alguna variable que le haya cambiado la descripcion pero que
+  # ya existia se va a quedar con la descripcion nueva. 
+  
+  etiquetas <- etiquetas %>% 
+    group_by(variable_nombre) %>% 
+    filter(if(n() == 1) row_number() == 1 else row_number() == n()) %>%
+    ungroup()
+  
+  etiquetas <- stats::setNames(as.list(etiquetas$descripcion), etiquetas$variable_nombre)
+  
+  return(etiquetas)
+  
+}
+
+# Tomo las variables output_name y subtopico declaradas arriba
+metadatos <- argendataR::metadata(subtopico = subtopico) %>% 
+  dplyr::filter(grepl(paste0("^", output_name), nombre_archivo)) %>% 
+  distinct(variable_nombre, descripcion) 
+
+
+
+# Guardo en una variable las columnas del output que queremos escribir
+output_cols <- names(df_output) # lo puedo generar así si tengo df_output
+
+
+
+descripcion <- armador_descripcion(metadatos = metadatos,
+                                   # etiquetas_nuevas = etiquetas_nuevas,
+                                   output_cols = output_cols)
+
+
+
+df_output %>%
+  argendataR::write_output(
+    output_name = output_name,
+    subtopico = subtopico,
+    control = comparacion, 
+    fuentes = argendataR::colectar_fuentes(),
+    analista = analista,
+    pk = pks,
+    descripcion_columnas = descripcion, 
+    unidad = list("exp_vida_al_nacer" = "unidades"))
+
+
+output_name <- gsub("\\.csv", "", output_name)
+mandar_data(paste0(output_name, ".csv"), subtopico = subtopico, branch = "main")
+mandar_data(paste0(output_name, ".json"), subtopico = subtopico,  branch = "main")
+
 
